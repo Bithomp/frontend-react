43bc302f3aa8545883e58ce3c99b278f5c56fe4f
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2022-04-12 15:10:53 +0200

    js alert fixup

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -65,14 +65,14 @@ function Home() {
       <div className="home-sponsored">
         <a href="https://bithomp.com/go/earn-on-xrp" target="_blank" rel="noreferrer">
           <div className="sponsored-brand">
-            <img src={nexo} className="sponsored-brand-icon" alt="nexo" />
+            <img src={nexo} className="sponsored-brand-icon" alt="earn on xrp" />
             <div className="sponsored-brand-title">Earn on XRP</div>
             <div className="sponsored-brand-text">Start earning up to 8% APR with XRP, paid out daily</div>
           </div>
         </a>
         <a href="https://bithomp.com/go/buy-xrp" target="_blank" rel="noreferrer">
           <div className="sponsored-brand">
-            <img src={btcbit} className="sponsored-brand-icon" alt="btcbit" />
+            <img src={btcbit} className="sponsored-brand-icon" alt="buy xrp" />
             <div className="sponsored-brand-title">Buy XRP</div>
             <div className="sponsored-brand-text">Instantly buy and sell cryptocurrency with low commission</div>
           </div>
