bf12f0bfadbfa03f41529b3c4669225b64809455
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2022-05-09 15:06:20 +0200

    disclaimer update

--- src/screens/Disclaimer.js
+++ src/screens/Disclaimer.js
@@ -6,6 +6,8 @@ function Disclaimer() {
       Bithomp will strive to ensure accuracy of information listed on this website although it will not hold any responsibility for any missing or wrong information. Data may be delayed or incorrect. Bithomp provides all information "as is" and solely for informational purposes only. You understand that you are using any and all information available here at your own risk.
       <h3>No advice</h3>
       The information provided on this website does not constitute investment advice, financial advice, trading advice, or any other sort of advice and you should not treat any of the website's content as such. Bithomp does not endorse, recommend, or make any representations with respect to the gateways and exchanges that appear on this website. Bithomp does not recommend that any cryptocurrency should be bought, sold, or held by you. Do conduct your own due diligence and consult your financial advisor before making any investment decisions.
+      <h3>No warranties</h3>
+      All content provided herein our website, hyperlinked sites, associated applications, forums, blogs, social media accounts and other platforms (“Site”) is for your general information only, procured primarily from third party sources. We make no warranties of any kind in relation to our content and services, including but not limited to accuracy, security and updatedness.
     </div>
   );
 }
