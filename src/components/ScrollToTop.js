45892cd79ae30b63673d996c1e6d27ce0a2d5cea
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2022-05-11 19:39:26 +0200

    Smooth scrolling on route change

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -5,7 +5,11 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }, [pathname]);
 
   return null;
