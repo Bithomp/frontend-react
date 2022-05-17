410647d25678f874d8dc67fce76daaa0ff024383
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2022-05-17 10:57:27 +0200

    i18n turn off debug info in console

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -16,7 +16,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'en',
-    debug: true,
+    debug: false,
     supportedLngs: ['en', 'ru'],
     interpolation: {
       escapeValue: false, // no need for react. it escapes by default
