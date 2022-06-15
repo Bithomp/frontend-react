af62fa73037d37450306c955a15d654a1c6482e3
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2022-06-15 23:28:50 +0200

    clean up

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -5,32 +5,8 @@ export default function Switch({ theme, switchTheme }) {
   return (
     <div className="switch">
       <div className={`switch-container ${theme}`} onClick={switchTheme}>
-        <img src={sun} className="switch-icon" alt="light mode" />
-        <img src={moon} className="switch-icon" alt="dark mode" />
-        <div className="credits">
-          <div>Sun Icon made by
-            <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a>
-            from
-            <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-            is licensed by
-            <a
-              href="http://creativecommons.org/licenses/by/3.0/"
-              title="Creative Commons BY 3.0"
-            >CC 3.0 BY</a>
-          </div>
-          <div>Moon Icon made by
-            <a
-              href="https://www.flaticon.com/authors/vectors-market"
-              title="Vectors Market">Vectors Market</a>
-            from
-            <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-            is licensed by
-            <a
-              href="http://creativecommons.org/licenses/by/3.0/"
-              title="Creative Commons BY 3.0"
-            >CC 3.0 BY</a>
-          </div>
-        </div>
+        <img src={sun} className="switch-icon sun" alt="light mode" />
+        <img src={moon} className="switch-icon moon" alt="dark mode" />
       </div>
     </div>
   );
