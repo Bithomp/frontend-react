704bdc1fb579a5e8b42514ee4070796cc25636b1
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2022-06-30 13:19:40 +0200

    NFT stats: show NFTs with auto trustline creation for sellers

--- src/screens/NftStatistics.js
+++ src/screens/NftStatistics.js
@@ -15,29 +15,37 @@ export default function LastLedgerInformation() {
     ws = new WebSocket(wssServer);
 
     ws.onopen = () => {
-      ws.send(JSON.stringify({ command: "subscribe", streams: ["statistics"], id: 1 }));
+      ws.send(JSON.stringify({ command: "subscribe", streams: ["nftokens"], id: 1 }));
     }
 
     ws.onmessage = evt => {
       const message = JSON.parse(evt.data);
       setData(message);
       setLastUpdate(new Date().toLocaleTimeString());
-      /* 
-      {
-        "nftokens":{
-          "created": 138633,
-          "burned": 9182,
-          "burnable": 21042,
-          "onlyXRP": 12602,
-          "transferable": 111820,
-          "owners": 11976,
-          "issuers": 13671,
-          "transfers": 16720,
-          "forSale": 4377,
-          "forSaleWithoutDestination": 2564,
-          "forSaleWithDestination": 1817
+      /*
+        {
+          "type": "NFTokens",
+          "validatedLedger": {
+            "age": 3,
+            "hash": "814430F058D4BBFB3677E6536EA61D0FA8125E18351FE47C467E09322F8BF5F5",
+            "ledgerTime": 1656405521,
+            "ledgerIndex": 3384852
+          },
+          "allTime": {
+            "created": 139470,
+            "burned": 9298,
+            "burnable": 21054,
+            "onlyXRP": 12607,
+            "trustline": 3233,
+            "transferable": 112271,
+            "owners": 12396,
+            "issuers": 14186,
+            "transfers": 18552,
+            "forSale": 5750,
+            "forSaleWithoutDestination": 3829,
+            "forSaleWithDestination": 1928
+          }
         }
-      }
       */
     }
 
@@ -55,7 +63,7 @@ export default function LastLedgerInformation() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const nft = data?.nftokens;
+  const nft = data?.allTime;
 
   return (
     <div className="content-text content-center">
@@ -63,6 +71,9 @@ export default function LastLedgerInformation() {
       <div className="main-box">
         <p>
           {t("nft-statistics.updated")}: {lastUpdate}
+        </p>
+        <p>
+          {t("nft-statistics.ledger-index")}: {data?.validatedLedger.ledgerIndex && '#' + data.validatedLedger.ledgerIndex}
         </p>
         <p>
           {t("nft-statistics.created")}: {numberWithSpaces(nft?.created)}
@@ -99,6 +110,9 @@ export default function LastLedgerInformation() {
         </p>
         <p>
           {t("nft-statistics.transferable")}: {numberWithSpaces(nft?.transferable)}
+        </p>
+        <p>
+          {t("nft-statistics.trustline")}: {numberWithSpaces(nft?.trustline)}
         </p>
         <p className="center" style={{ position: "absolute", top: "calc(50% - 72px)", left: "calc(50% - 54px)" }}>
           {!data && <span className="waiting"></span>}
