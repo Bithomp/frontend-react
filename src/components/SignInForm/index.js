f89098828072f1367d01739055130f291aa2dc72
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2022-09-04 12:12:30 +0300

    links and translation

--- src/components/SignInForm/index.js
+++ src/components/SignInForm/index.js
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 import './styles.scss';
 
 import xumm from '../../assets/images/xumm-large.svg';
@@ -7,22 +9,24 @@ import ellipal from '../../assets/images/ellipal-large.svg';
 
 
 export default function SignInForm({ setSignInFormOpen }) {
+  const { t } = useTranslation();
+
   return (
     <div className="sign-in-form">
       <div className="sign-in-body center">
-        <div className='header'>Choose a signing app</div>
+        <div className='header'>{t("signin.choose-app")}</div>
         <div className='close-button' onClick={() => setSignInFormOpen(false)}></div>
         <div className='signin-apps'>
-          <a href="">
+          <a href="/explorer/?hwlogin=xumm">
             <img alt="xumm" className='signin-app-logo' src={xumm} />
           </a>
-          <a href="">
+          <a href="/explorer/?hwlogin=ledger">
             <img alt="ledger" className='signin-app-logo' src={ledger} />
           </a>
-          <a href="">
+          <a href="/explorer/?hwlogin=trezor">
             <img alt="trezor" className='signin-app-logo' src={trezor} />
           </a>
-          <a href="">
+          <a href="/explorer/?hwlogin=ellipal">
             <img alt="ellipal" className='signin-app-logo' src={ellipal} />
           </a>
         </div>
