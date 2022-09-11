4d5f0529b134cc14b63586125017e5be92371681
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2022-09-11 15:43:59 +0200

    return 0 in numberWithSpaces

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 export const numberWithSpaces = (x) => {
   if (!x) {
-    return '';
+    return x === 0 ? 0 : '';
   }
   var parts = x.toString().split(".");
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
