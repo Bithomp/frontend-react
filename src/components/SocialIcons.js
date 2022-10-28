3bb538f28e72aef11395ea6accb652fca7f734ea
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2022-10-28 07:14:03 +0300

    Youtube link update

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -8,7 +8,7 @@ export default function SocialIcons() {
   return (
     <>
       <a href="https://twitter.com/bithomp"><img src={twitter} className="footer-social-icon" alt="twitter" /></a>
-      <a href="https://www.youtube.com/channel/UCTvrMnG-Tpqi5FN9zO7GcWw"><img src={youtube} className="footer-social-icon" alt="youtube" /></a>
+      <a href="https://youtube.com/@bithomp"><img src={youtube} className="footer-social-icon" alt="youtube" /></a>
       <a href="https://www.instagram.com/bithomp/"><img src={instagram} className="footer-social-icon" alt="instagram" /></a>
       <a href="https://www.reddit.com/user/bithomp/"><img src={reddit} className="footer-social-icon" alt="reddit" /></a>
       <a href="https://github.com/Bithomp"><img src={github} className="footer-social-icon" alt="github" /></a>
