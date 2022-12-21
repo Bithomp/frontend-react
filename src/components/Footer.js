4cb5e880023241bbc3130febbc82c74baee11bc6
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2022-12-21 13:19:31 +0100

    Add buy xrp link in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -67,6 +67,7 @@ export default function Footer() {
 
         {!devNet && <div className="footer-menu-column">
           <span className="footer-menu-header">{t("menu.sponsored.title")}</span>
+          <a href="/go/buy-xrp" target="_blank" rel="noreferrer">{t("menu.sponsored.buy")}</a>
           <a href="/go/earn-on-xrp" target="_blank" rel="noreferrer">{t("menu.sponsored.earn")}</a>
           <a href="/go/playxrp" target="_blank" rel="noreferrer">{t("menu.sponsored.play")}</a>
         </div>}
