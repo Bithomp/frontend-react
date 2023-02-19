cfa83fc23a7356f0905e317dfb0c7bff6732c759
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2023-02-19 23:24:23 +0100

    Whales XRP only

--- src/screens/Home/Whales.js
+++ src/screens/Home/Whales.js
@@ -92,7 +92,7 @@ export default function Whales({ currency }) {
         <div key={tx.hash} className={"tx-row" + (difference?.includes(tx) ? " just-added" : "")}>
           <span className='tx-time'>{timeFormat(tx.timestamp)}</span>
           <span className='tx-link'><a href={'/explorer/' + tx.hash}>{txIdFormat(tx.hash)}</a></span>
-          <span className='tx-amount'>{shortNiceNumber(tx.amount, 0, 1)} {tx.currency}</span>
+          <span className='tx-amount'>{shortNiceNumber(tx.amountXRP, 0, 1)} XRP</span>
           <span className='tx-amount-fiat'>{devNet ? t("home.whales.no-value") : (tx.amountFiats ? shortNiceNumber(tx.amountFiats[currency?.toLowerCase()], 0, 1, currency) : "")}</span>
         </div>
       ))}
