882a1768ee2ba9398394c14bd22a44c532a0e36c
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2023-04-11 15:44:00 +0200

    Donations, show list of latest transactions

--- pages/donate.js
+++ pages/donate.js
@@ -1,10 +1,18 @@
 import { useTranslation } from 'next-i18next'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Image from 'next/image'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-export async function getStaticProps({ locale }) {
+import { useIsMobile, getIsSsrMobile } from "../utils/mobile"
+import { fullDateAndTime, amountFormat, addressUsernameOrServiceLink } from '../utils/format'
+import { stripText } from '../utils'
+
+export async function getServerSideProps(context) {
+  const { locale } = context
   return {
     props: {
+      isSsrMobile: getIsSsrMobile(context),
       ...(await serverSideTranslations(locale, ['common'])),
     }
   }
@@ -13,8 +21,45 @@ export async function getStaticProps({ locale }) {
 import CopyButton from '../components/UI/CopyButton'
 import SEO from '../components/SEO'
 
+const showTable = false
+
 export default function Donate() {
   const { t } = useTranslation()
+  const isMobile = useIsMobile()
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      /*
+      "transactions": [
+        {
+          "processedAt": 1680450744,
+          "hash": "3B5F22B245069E1FB4A83A668D71E7EB687D10081A6F48514EB3A64D2E420330",
+          "ledger": 78833252,
+          "type": "Payment",
+          "sourceAddress": "rf1NrYAsv92UPDd8nyCG4A3bez7dhYE61r",
+          "destinationAddress": "rPPHhfSQbHt1t2XPHWAK1HTcjqDg56TzZy",
+          "destinationTag": 1,
+          "amount": 50,
+          "memos": [
+            {
+              "type": "Description",
+              "format": "text/plain",
+              "data": "Donated by Ekiserrepe.com"
+            }
+          ],
+          "sourceAddressDetails": {
+            "username": "ekiserrepe",
+            "service": "Ekiserrepe"
+          }
+        },
+      */
+      const response = await axios('v2/donations')
+      setData(response.data)
+    }
+    fetchData();
+  }, [setData])
 
   return <>
     <SEO title={t("menu.donate") + " ❤"} />
@@ -44,6 +89,62 @@ export default function Donate() {
           <b>1</b> <CopyButton text="1" />
         </div>
       </div>
+      <br /><br />
+
+      {showTable && data?.transactions && <>
+        {isMobile ?
+          <table className="table-mobile">
+            <thead>
+            </thead>
+            <tbody>
+              {data?.transactions?.map((tx, i) => (
+                <tr key={i}>
+                  <td style={{ paddingLeft: "15px" }}>
+                    <p>
+                      {t("table.date")}<br />
+                      {fullDateAndTime(tx.processedAt)}
+                    </p>
+                    <p>
+                      {t("table.sender")}<br />
+                      {addressUsernameOrServiceLink(tx, 'sourceAddress', { short: true })}
+                    </p>
+                    <p>
+                      {t("table.amount")}<br />
+                      {amountFormat(tx.amount)}
+                    </p>
+                    <p>
+                      {t("table.memo")}<br />
+                      {tx.memos && tx.memos.length > 0 && stripText(tx.memos[0]?.data)}
+                    </p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          :
+          <table className="table-large">
+            <thead>
+              <tr>
+                <th>{t("table.date")}</th>
+                <th>{t("table.sender")}</th>
+                <th>{t("table.amount")}</th>
+                <th>{t("table.memo")}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.transactions?.map((tx, i) =>
+                <tr key={i}>
+                  <td>{fullDateAndTime(tx.processedAt)}</td>
+                  <td>{addressUsernameOrServiceLink(tx, 'sourceAddress', { short: true })}</td>
+                  <td>{amountFormat(tx.amount)}</td>
+                  <td>{tx.memos && tx.memos.length > 0 && stripText(tx.memos[0]?.data)}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        }
+      </>
+      }
     </div>
-  </>;
-};
+  </>
+}
