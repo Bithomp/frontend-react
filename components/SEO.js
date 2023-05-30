30e13082ce8a597793566ed18e33bbc0a5888bb7
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2023-05-30 14:23:29 +0200

    Catalan language by github account @EnigmaRidworld
    Catalan language by github account @EnigmaRidworld

--- components/SEO.js
+++ components/SEO.js
@@ -53,7 +53,8 @@ export default function SEO({ title, description, image, page, images, websiteNa
   let languageAlternates = [
     { hrefLang: 'en', href: server + path },
     { hrefLang: 'ru', href: server + '/ru' + path },
-    { hrefLang: 'es', href: server + '/es' + path }
+    { hrefLang: 'es', href: server + '/es' + path },
+    { hrefLang: 'ca', href: server + '/ca' + path }
   ]
 
   return (
--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'ru', 'es'],
+    locales: ['en', 'ru', 'es', 'ca'],
   },
   reloadOnPrerender: process.env.NODE_ENV === 'development'
 }
