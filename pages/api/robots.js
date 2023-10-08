2df6b28bc91e733479f4b038164ce546d49ecd4e
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2023-10-08 09:21:49 +0200

    allow indexing testnets

--- pages/api/robots.js
+++ pages/api/robots.js
@@ -1,8 +1,12 @@
 export default function robots(req, res) {
   res.setHeader("Content-Type", "text/plain")
+  //let allow indexing all networks for now
+  res.send('User-agent: *\nDisallow: /api/\nDisallow: /go/')
+  /*
   if (process.env.NEXT_PUBLIC_NETWORK_NAME === "mainnet") {
     res.send('User-agent: *\nDisallow: /api/\nDisallow: /go/')
   } else {
     res.send('User-agent: *\nAllow: /$\nDisallow: /\nDisallow: /go/')
   }
+  */
 }
