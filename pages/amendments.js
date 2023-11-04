d024f82caeffa17530cc029872d4ab6fc48b3db0
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2023-11-04 16:26:57 +0300

    Amendments: show disabled amendments

--- pages/amendments.js
+++ pages/amendments.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { fullDateAndTime, shortHash } from '../utils/format'
+import { useWidth } from '../utils'
 
 import SEO from '../components/SEO'
 import CopyButton from '../components/UI/CopyButton'
@@ -11,22 +12,31 @@ import CopyButton from '../components/UI/CopyButton'
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common'])),
+      ...(await serverSideTranslations(locale, ['common', 'amendments'])),
     }
   }
 }
 
 export default function Amendment() {
-  const [majorityAmendments, setMajorityAmendments] = useState(null);
-  const [enabledAmendments, setEnabledAmendments] = useState(null);
-  const { t } = useTranslation();
+  const windowWidth = useWidth()
+  const [majorityAmendments, setMajorityAmendments] = useState(null)
+  const [enabledAmendments, setEnabledAmendments] = useState(null)
+  const { t } = useTranslation(['common', 'amendments'])
+
+  const [disabledAmendments, setDisabledAmendments] = useState(null)
+
+  //enabled
+  //voting
+  //reached majority
+  //obsolete
 
   const checkApi = async () => {
-    const response = await axios('v2/amendment');
-    const data = response.data;
+    const response = await axios('v2/amendment')
+    const data = response.data //.sort(a => (!a.introduced) ? -1 : 1)
     if (data) {
-      setMajorityAmendments(data.filter(a => !!a.majority));
-      setEnabledAmendments(data.filter(a => a.enabled === true));
+      setDisabledAmendments(data.filter(a => a.enabled === false))
+      setMajorityAmendments(data.filter(a => !!a.majority))
+      setEnabledAmendments(data.filter(a => a.enabled === true))
     }
   }
 
@@ -56,53 +66,88 @@ export default function Amendment() {
   return <>
     <SEO title={t("menu.xrpl.amendments")} />
     <div className="content-text">
-      {majorityAmendments?.length ?
+      {majorityAmendments?.length > 0 &&
         <>
-          <h1 className="center">{t("amendment.soon")}</h1>
-          <table className="table-large shrink">
+          <h1 className="center">{t("soon", { ns: 'amendments' })}</h1>
+          <table className="table-large">
             <thead>
               <tr>
-                <th>{t("amendment.name")}</th>
-                <th>{t("amendment.introduction")}</th>
-                <th>{t("amendment.majority")}</th>
-                <th>{t("amendment.eta")}</th>
+                <th className='center'>{t("table.index")}</th>
+                <th>{t("table.name")}</th>
+                <th className='right'>{t("version", { ns: 'amendments' })}</th>
+                <th>{t("majority", { ns: 'amendments' })}</th>
+                <th>{t("eta", { ns: 'amendments' })}</th>
               </tr>
             </thead>
             <tbody>
-              {majorityAmendments.map(a =>
+              {majorityAmendments.map((a, i) =>
                 <tr key={a.amendment}>
+                  <td className='center'>{i + 1}</td>
                   <td className="brake">{amendmentLink(a.name, a.amendment)}</td>
-                  <td className='center'>{a.introduced}</td>
+                  <td className='right'>{a.introduced}</td>
                   <td>{fullDateAndTime(a.majority)}</td>
                   <td>{fullDateAndTime(a.majority + 14 * 86400 + 3)}</td>
                 </tr>
               )}
             </tbody>
           </table>
-        </> : ""
+        </>
       }
-      {enabledAmendments?.length ?
+      {enabledAmendments?.length > 0 &&
         <>
-          <h1 className="center">{t("amendment.enabled")}</h1>
-          <table className="table-large shrink">
+          <h1 className="center">{t("enabled", { ns: 'amendments' })}</h1>
+          <table className="table-large">
             <thead>
               <tr>
-                <th>{t("amendment.name")}</th>
-                <th>{t("table.hash")}</th>
-                <th className='right'>{t("amendment.introduction")}</th>
+                <th className='center'>{t("table.index")}</th>
+                <th>{t("table.name")}</th>
+                <th className='right'>{t("version", { ns: 'amendments' })}</th>
+                <th className='right'>{t("table.hash")}</th>
               </tr>
             </thead>
             <tbody>
-              {enabledAmendments.map(a =>
+              {enabledAmendments.map((a, i) =>
                 <tr key={a.amendment}>
+                  <td className='center'>{i + 1}</td>
                   <td>{amendmentLink(a.name, a.amendment)}</td>
-                  <td><CopyButton text={a.amendment} /> {shortHash(a.amendment)}</td>
                   <td className='right'>{a.introduced}</td>
+                  <td className='right'>
+                    {windowWidth > 1000 ? <>{shortHash(a.amendment)} </> : ""}
+                    <CopyButton text={a.amendment} />
+                  </td>
                 </tr>
               )}
             </tbody>
           </table>
-        </> : ""
+        </>
+      }
+      {disabledAmendments?.length > 0 &&
+        <>
+          <h1 className="center">Disabled amendments</h1>
+          <table className="table-large">
+            <thead>
+              <tr>
+                <th className='center'>{t("table.index")}</th>
+                <th>{t("table.name")}</th>
+                <th className='right'>{t("version", { ns: 'amendments' })}</th>
+                <th className='right'>{t("table.hash")}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {disabledAmendments.map((a, i) =>
+                <tr key={a.amendment}>
+                  <td className='center'>{i + 1}</td>
+                  <td className="brake">{amendmentLink(a.name, a.amendment)}</td>
+                  <td className='right'>{a.introduced}</td>
+                  <td className='right'>
+                    {windowWidth > 1000 ? <>{shortHash(a.amendment)} </> : ""}
+                    <CopyButton text={a.amendment} />
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </>
       }
     </div>
   </>;
