3f7c0e5484ee6a2dac223c55d79deca7cb8c2402
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-01-18 00:57:16 +0100

    js build notification fix

--- components/Transaction/index.js
+++ components/Transaction/index.js
@@ -3,4 +3,4 @@ export { TransactionDetails } from "./TransactionDetails";
 export { TransactionEscrowCreation } from "./TransactionEscrowCreation";
 export { TransactionOrder } from "./TransactionOrder";
 export { TransactionPayment } from "./TransactionPayment";
-export { LinkAccount, LinkExplorer, LinkLedger } from "./Links";
+export { LinkAccount, LinkLedger } from "./Links";
