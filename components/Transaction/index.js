56339f195f052f0790589c2d596c3d4e762df0e4
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-01-28 19:58:08 +0100

    transaction: build fix

--- components/Transaction/index.js
+++ components/Transaction/index.js
@@ -1,6 +1,5 @@
 export { TransactionCard } from "./TransactionCard";
 export { TransactionDetails } from "./TransactionDetails";
-export { TransactionEscrowCreation } from "./TransactionEscrowCreation";
 export { TransactionEscrow } from "./TransactionEscrow";
 export { TransactionOrder } from "./TransactionOrder";
 export { TransactionPayment } from "./TransactionPayment";
