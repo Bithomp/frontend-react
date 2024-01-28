41b5c94fb01fa6b97d8b070c47d086bbbcb25de6
Pavel Fokin <fokinpv@gmail.com>
2024-01-28 19:30:37 +0100

    Add transaction card for escrows (#100)

--- components/Transaction/index.js
+++ components/Transaction/index.js
@@ -1,6 +1,7 @@
 export { TransactionCard } from "./TransactionCard";
 export { TransactionDetails } from "./TransactionDetails";
 export { TransactionEscrowCreation } from "./TransactionEscrowCreation";
+export { TransactionEscrow } from "./TransactionEscrow";
 export { TransactionOrder } from "./TransactionOrder";
 export { TransactionPayment } from "./TransactionPayment";
 export { LinkAccount, LinkLedger } from "./Links";
