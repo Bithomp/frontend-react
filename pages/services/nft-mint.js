4355e7da12a0f47242af53284fd6dca2e4f840f4
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-01-31 10:55:07 +0100

    UriTokenMint - allow 512 characters in the URI

--- pages/services/nft-mint.js
+++ pages/services/nft-mint.js
@@ -269,7 +269,7 @@ export default function NftMint({ setSignRequest, uriQuery, digestQuery }) {
               className="input-text"
               ref={node => { uriRef = node; }}
               spellCheck="false"
-              maxLength="256"
+              maxLength="512"
               name="uri"
             />
           </div>
