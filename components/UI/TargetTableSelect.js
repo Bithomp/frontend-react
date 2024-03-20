01113601177c2813b7ebfda0650c42fb87fdfbfe
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-03-20 15:49:18 +0100

    fix for VSC import suggestions: react-i18n -> next-i18n

--- components/UI/TargetTableSelect.js
+++ components/UI/TargetTableSelect.js
@@ -1,5 +1,5 @@
 import Select from 'react-select'
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "next-i18next"
 
 export default function TargetTableSelect({ onChange, layer = 1 }) {
   const { t } = useTranslation()
