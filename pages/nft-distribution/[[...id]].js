197320ab4332ee891a9b6bc7bf2d252b0fa17c9a
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-04-10 21:03:02 +0200

    NFT-distribution: add sorting

--- pages/nft-distribution/[[...id]].js
+++ pages/nft-distribution/[[...id]].js
@@ -5,7 +5,15 @@ import { CSVLink } from "react-csv"
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useRouter } from 'next/router'
 
-import { useWidth, isAddressOrUsername, addAndRemoveQueryParams } from '../../utils'
+import { FaSortAmountDown } from "react-icons/fa"
+
+import {
+  useWidth,
+  isAddressOrUsername,
+  addAndRemoveQueryParams,
+  addQueryParams,
+  removeQueryParams
+} from '../../utils'
 import { isValidTaxon } from '../../utils/nft'
 import {
   nftsExplorerLink,
@@ -18,7 +26,7 @@ import DownloadIcon from "../../public/images/download.svg"
 
 export async function getServerSideProps(context) {
   const { locale, query } = context
-  const { taxon, issuer, id } = query
+  const { taxon, issuer, id, order } = query
   //keep query instead of params, anyway it is an array sometimes
   const idQuery = id ? (Array.isArray(id) ? id[0] : id) : ""
   let issuerQuery = isAddressOrUsername(idQuery) ? idQuery : issuer
@@ -27,6 +35,7 @@ export async function getServerSideProps(context) {
     props: {
       idQuery,
       issuerQuery,
+      orderQuery: order || "ownerAndNotMinter",
       taxonQuery: taxon || "",
       ...(await serverSideTranslations(locale, ['common']))
     }
@@ -35,7 +44,7 @@ export async function getServerSideProps(context) {
 
 import SEO from '../../components/SEO'
 
-export default function NftDistribution({ issuerQuery, taxonQuery, idQuery }) {
+export default function NftDistribution({ issuerQuery, taxonQuery, idQuery, orderQuery }) {
   const { t } = useTranslation()
   const windowWidth = useWidth()
   const router = useRouter()
@@ -47,6 +56,7 @@ export default function NftDistribution({ issuerQuery, taxonQuery, idQuery }) {
   const [issuer, setIssuer] = useState(issuerQuery)
   const [issuerInput, setIssuerInput] = useState(issuerQuery)
   const [taxonInput, setTaxonInput] = useState(taxonQuery)
+  const [order, setOrder] = useState(orderQuery)
 
   const checkApi = async () => {
     /*
@@ -73,14 +83,21 @@ export default function NftDistribution({ issuerQuery, taxonQuery, idQuery }) {
     if (taxon > -1) {
       taxonUrlPart = '&taxon=' + taxon
     }
-    //order=ownerAndNotMinter, marker=m
+    //marker=m
+    setLoading(true)
+
+    let orderPart = order
+    if (issuer) {
+      orderPart = 'owner'
+    }
+
     const response = await axios(
-      'v2/nft-owners?issuer=' + issuer + taxonUrlPart
+      'v2/nft-owners?issuer=' + issuer + taxonUrlPart + '&order=' + orderPart
     ).catch(error => {
       setErrorMessage(t("error." + error.message))
     })
-    setLoading(false);
-    const newdata = response?.data;
+    setLoading(false)
+    const newdata = response?.data
     if (newdata) {
       if (newdata.owners) {
         if (newdata.owners.length > 0) {
@@ -103,7 +120,7 @@ export default function NftDistribution({ issuerQuery, taxonQuery, idQuery }) {
   useEffect(() => {
     checkApi()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [issuer, taxon])
+  }, [issuer, taxon, order])
 
   let csvHeaders = [
     { label: t("table.owner"), key: "owner" },
@@ -155,6 +172,15 @@ export default function NftDistribution({ issuerQuery, taxonQuery, idQuery }) {
       e.preventDefault()
     }
     enterPress(e)
+  }
+
+  const changeOrder = order => {
+    setOrder(order)
+    if (order === 'owner') {
+      addQueryParams(router, [{ name: "order", value: "owner" }])
+    } else {
+      removeQueryParams(router, ["order"])
+    }
   }
 
   return <>
@@ -218,12 +244,32 @@ export default function NftDistribution({ issuerQuery, taxonQuery, idQuery }) {
             <th>{t("table.owner")}</th>
             {!issuer &&
               <>
-                <th className='right'>Not self-issued</th>
-                <th className='right'>Self-issued</th>
+                <th className='right'>
+                  <span
+                    onClick={() => changeOrder('ownerAndNotMinter')}
+                    style={order !== 'ownerAndNotMinter' ? { cursor: "pointer" } : {}}
+                    className={order === 'ownerAndNotMinter' ? "blue" : ""}
+                  >
+                    Non-self-issued <FaSortAmountDown />
+                  </span>
+                </th>
+                <th className='right'>
+                  Self-issued
+                </th>
               </>
             }
             <th className='right'>
-              {issuer ? t("table.nfts") : "Total"}
+              {issuer ?
+                t("table.nfts")
+                :
+                <span
+                  onClick={() => changeOrder('owner')}
+                  style={order !== 'owner' ? { cursor: "pointer" } : {}}
+                  className={order === 'owner' ? "blue" : ""}
+                >
+                  Total <FaSortAmountDown />
+                </span>
+              }
             </th>
           </tr>
         </thead>
