bd7778363096f588dacb8315ac03c56fd45b860e
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-04-23 11:58:44 +0200

    Revert "Revert "Menu fix (when signed-in)""
    This reverts commit 919c7c2434656597c496563939f80cc8281979a3.

--- components/Layout/Header/index.js
+++ components/Layout/Header/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { useTranslation } from 'next-i18next'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 
 import { devNet, xahauNetwork, ledgerName, nativeCurrency, network } from '../../../utils'
 
@@ -20,45 +20,34 @@ export default function Header({ setSignRequest, account, signOut, selectedCurre
 
   const [xummUserToken, setXummUserToken] = useState(null)
 
-  const [hoverStates, setHoverStates] = useState({});
-  const timeoutIds = useRef({});
+  const [hoverStates, setHoverStates] = useState({})
+  let timeoutIds = {}
 
-  const handleMouseEnter = (id) => () => {
-    Object.values(timeoutIds.current).forEach(() => {
-      clearTimeout(timeoutIds.current[id]);
-    });
-    setHoverStates(prevState => {
+  const handleMouseEnter = id => {
+    // clear timeout for all dropdowns
+    for (let key in timeoutIds) {
+      clearTimeout(timeoutIds[key])
+    }
+    //hide other dropdowns, show this one
+    setHoverStates(state => {
       const newState = {};
-      Object.keys(prevState).forEach(key => {
-        newState[key] = false;
+      Object.keys(state).forEach(key => {
+        newState[key] = false
       });
-      newState[id] = true;
+      newState[id] = true
       return newState;
     });
   };
 
-  const handleMouseLeave = (id) => () => {
-    timeoutIds.current[id] = setTimeout(() => {
-      setHoverStates(prevState => ({ ...prevState, [id]: false }));
-    }, 800);
-  };
+  const handleMouseLeave = id => {
+    timeoutIds[id] = setTimeout(() => {
+      setHoverStates(state => ({ ...state, [id]: false }))
+    }, 600)
+  }
 
   useEffect(() => {
     setRendered(true)
     setXummUserToken(localStorage.getItem('xummUserToken'))
-
-    const dropdowns = document.querySelectorAll('.menu-dropdown');
-    dropdowns.forEach((dropdown) => {
-      dropdown.addEventListener('mouseenter', handleMouseEnter(dropdown.id));
-      dropdown.addEventListener('mouseleave', handleMouseLeave(dropdown.id));
-    });
-
-    return () => {
-      dropdowns.forEach((dropdown) => {
-        dropdown.removeEventListener('mouseenter', handleMouseEnter(dropdown.id));
-        dropdown.removeEventListener('mouseleave', handleMouseLeave(dropdown.id));
-      });
-    };
   }, [])
 
   let address, hashicon, displayName, username;
@@ -101,7 +90,24 @@ export default function Header({ setSignRequest, account, signOut, selectedCurre
       (err) => {
         console.error('Could not copy text: ', err);
       }
-    );
+    )
+  }
+
+  const MenuDropDown = ({ children, id, title }) => {
+    return (
+      <div
+        className="menu-dropdown"
+        onMouseEnter={() => handleMouseEnter(id)}
+        onMouseLeave={() => handleMouseLeave(id)}
+      >
+        <div className="menu-dropdown-button">{title}</div>
+        {hoverStates[id] &&
+          <div className="menu-dropdown-content">
+            {children}
+          </div>
+        }
+      </div>
+    )
   }
 
   return (
@@ -109,155 +115,137 @@ export default function Header({ setSignRequest, account, signOut, selectedCurre
       <header>
         <Link href="/"><div className='header-logo'><LogoAnimated /></div></Link>
         <div className="header-menu-left">
-          <div className="menu-dropdown" id="dropdown1">
-            <div className="menu-dropdown-button">{t("menu.personal.personal")}</div>
-            <div className="menu-dropdown-content" style={{ display: hoverStates["dropdown1"] ? 'block' : 'none' }}>
-              <a href={"/explorer/"}>{t("menu.personal.search-on-ledgerName", { ledgerName })}</a>
-              <Link href="/username">{t("menu.usernames")}</Link>
-              {displayName ?
-                <Link href={"/nfts/" + address}>{t("signin.actions.my-nfts")}</Link>
-                :
-                <span onClick={() => { setSignRequest({ redirect: "nfts" }) }} className="link">{t("signin.actions.my-nfts")}</span>
-              }
-              {!devNet && <Link href="/alerts">{t("menu.price-alerts", { nativeCurrency })}</Link>}
-              {!devNet && <a href={"/submit/"}>{t("menu.submit-offline-tx")}</a>}
-            </div>
-          </div>
 
-          <div className="menu-dropdown" id="dropdown2">
-            <div className="menu-dropdown-button">{t("menu.business.business")}</div>
-            <div className="menu-dropdown-content" style={{ display: hoverStates["dropdown2"] ? 'block' : 'none' }}>
-              <Link href="/advertise">{t("menu.business.advertise")}</Link>
-              <Link href="/username">{t("menu.usernames")}</Link>
-              <Link href="/submit-account-information">{t("menu.project-registration")}</Link>
-              <a href="https://docs.bithomp.com">{t("menu.developers.api")}</a>
-              <Link href="/eaas">{t("menu.business.eaas")}</Link>
-              <Link href="/build-unl">{t("menu.business.build-unl")}</Link>
-              <Link href="/press">{t("menu.press")}</Link>
-            </div>
-          </div>
+          <MenuDropDown id="dropdown1" title={t("menu.personal.personal")}>
+            <a href={"/explorer/"}>{t("menu.personal.search-on-ledgerName", { ledgerName })}</a>
+            <Link href="/username">{t("menu.usernames")}</Link>
+            {displayName ?
+              <Link href={"/nfts/" + address}>{t("signin.actions.my-nfts")}</Link>
+              :
+              <span onClick={() => { setSignRequest({ redirect: "nfts" }) }} className="link">{t("signin.actions.my-nfts")}</span>
+            }
+            {!devNet && <Link href="/alerts">{t("menu.price-alerts", { nativeCurrency })}</Link>}
+            {!devNet && <a href={"/submit/"}>{t("menu.submit-offline-tx")}</a>}
+          </MenuDropDown>
 
-          <div className="menu-dropdown" id="dropdown3">
-            <div className="menu-dropdown-button">NFT</div>
-            <div className="menu-dropdown-content" style={{ display: hoverStates["dropdown3"] ? 'block' : 'none' }}>
-              <Link href="/nft-explorer">{t("menu.nft.explorer")}</Link>
-              {/* Hide NFT menu for XAHAU while they are not ready yet */}
-              {!xahauNetwork && <>
-                <Link href="/nft-volumes">{t("menu.nft.volumes")}</Link>
-                <Link href="/nft-sales">{t("menu.nft.sales")}</Link>
-                <Link href="/nft-minters">{t("menu.nft.minters")}</Link>
-              </>
-              }
+          <MenuDropDown id="dropdown2" title={t("menu.business.business")}>
+            <Link href="/advertise">{t("menu.business.advertise")}</Link>
+            <Link href="/username">{t("menu.usernames")}</Link>
+            <Link href="/submit-account-information">{t("menu.project-registration")}</Link>
+            <a href="https://docs.bithomp.com">{t("menu.developers.api")}</a>
+            <Link href="/eaas">{t("menu.business.eaas")}</Link>
+            <Link href="/build-unl">{t("menu.business.build-unl")}</Link>
+            <Link href="/press">{t("menu.press")}</Link>
+          </MenuDropDown>
 
-              <Link href={"/nfts" + (displayName ? ("/" + address) : "")}>
-                {t("menu.nft.nfts")}
+          <MenuDropDown id="dropdown3" title="NFT">
+            <Link href="/nft-explorer">{t("menu.nft.explorer")}</Link>
+            {/* Hide NFT menu for XAHAU while they are not ready yet */}
+            {!xahauNetwork && <>
+              <Link href="/nft-volumes">{t("menu.nft.volumes")}</Link>
+              <Link href="/nft-sales">{t("menu.nft.sales")}</Link>
+              <Link href="/nft-minters">{t("menu.nft.minters")}</Link>
+            </>
+            }
+
+            <Link href={"/nfts" + (displayName ? ("/" + address) : "")}>
+              {t("menu.nft.nfts")}
+            </Link>
+
+            {/* Hide NFT menu for XAHAU while they are not ready yet */}
+            {!xahauNetwork && <>
+              <Link href={"/nft-offers" + (displayName ? ("/" + address) : "")}>
+                {t("menu.nft.offers")}
               </Link>
-
-              {/* Hide NFT menu for XAHAU while they are not ready yet */}
-              {!xahauNetwork && <>
-                <Link href={"/nft-offers" + (displayName ? ("/" + address) : "")}>
-                  {t("menu.nft.offers")}
-                </Link>
-                <Link href="/nft-distribution">{t("menu.nft.distribution")}</Link>
-                <Link href="/nft-statistics">{t("menu.nft.statistics")}</Link>
-              </>
-              }
-              {xahauNetwork &&
-                <Link href="/services/nft-mint">{t("menu.services.nft-mint")}</Link>
-              }
-            </div>
-          </div>
+              <Link href="/nft-distribution">{t("menu.nft.distribution")}</Link>
+              <Link href="/nft-statistics">{t("menu.nft.statistics")}</Link>
+            </>
+            }
+            {xahauNetwork &&
+              <Link href="/services/nft-mint">{t("menu.services.nft-mint")}</Link>
+            }
+          </MenuDropDown>
 
           {/* Hide AMM for XAHAU */}
           {!xahauNetwork &&
-            <div className="menu-dropdown" id="dropdown4">
-              <div className="menu-dropdown-button">{t("menu.amm.amm")}</div>
-              <div className="menu-dropdown-content" style={{ display: hoverStates["dropdown4"] ? 'block' : 'none' }}>
-                <Link href="/amms">{t("menu.amm.pools")}</Link>
-                <Link href="/amm">{t("menu.amm.explorer")}</Link>
-              </div>
-            </div>
+            <MenuDropDown id="dropdown4" title={t("menu.amm.amm")}>
+              <Link href="/amms">{t("menu.amm.pools")}</Link>
+              <Link href="/amm">{t("menu.amm.explorer")}</Link>
+            </MenuDropDown>
           }
 
-          <div className="menu-dropdown" id="dropdown5">
-            <div className="menu-dropdown-button">{ledgerName}</div>
-            <div className="menu-dropdown-content" style={{ display: hoverStates["dropdown5"] ? 'block' : 'none' }}>
-              {xahauNetwork && <Link href="/governance">{t("menu.xrpl.governance")}</Link>}
-              <Link href="/activations">{t("menu.xrpl.activations")}</Link>
-              <Link href="/distribution">{t("menu.xrpl.distribution", { nativeCurrency })}</Link>
-              <Link href="/last-ledger-information">{t("menu.xrpl.last-ledger-information")}</Link>
-              <Link href="/ledger">{t("menu.xrpl.last-ledger-transactions")}</Link>
-              {/* Hide Verified Domains for XAHAU while they are not ready yet */}
-              {!xahauNetwork &&
-                <Link href="/domains">{t("menu.xrpl.verified-domains")}</Link>
-              }
-              <Link href="/validators">{t("menu.xrpl.validators")}</Link>
-              <Link href="/amendments">{t("menu.xrpl.amendments")}</Link>
-              {xahauNetwork && <Link href="/unl-report">{t("menu.xrpl.unl-report")}</Link>}
-              <Link href="/genesis">{t("menu.xrpl.genesis")}</Link>
-            </div>
-          </div>
+          <MenuDropDown id="dropdown5" title={ledgerName}>
+            {xahauNetwork && <Link href="/governance">{t("menu.xrpl.governance")}</Link>}
+            <Link href="/activations">{t("menu.xrpl.activations")}</Link>
+            <Link href="/distribution">{t("menu.xrpl.distribution", { nativeCurrency })}</Link>
+            <Link href="/last-ledger-information">{t("menu.xrpl.last-ledger-information")}</Link>
+            <Link href="/ledger">{t("menu.xrpl.last-ledger-transactions")}</Link>
+            {/* Hide Verified Domains for XAHAU while they are not ready yet */}
+            {!xahauNetwork &&
+              <Link href="/domains">{t("menu.xrpl.verified-domains")}</Link>
+            }
+            <Link href="/validators">{t("menu.xrpl.validators")}</Link>
+            <Link href="/amendments">{t("menu.xrpl.amendments")}</Link>
+            {xahauNetwork && <Link href="/unl-report">{t("menu.xrpl.unl-report")}</Link>}
+            <Link href="/genesis">{t("menu.xrpl.genesis")}</Link>
+          </MenuDropDown>
 
-          <div className="menu-dropdown" id="dropdown6">
-            <div className="menu-dropdown-button">{t("menu.developers.developers")}</div>
-            <div className="menu-dropdown-content" style={{ display: hoverStates["dropdown6"] ? 'block' : 'none' }}>
-              {devNet &&
-                <>
-                  <a href={"/create/"}>{t("menu.developers.account-generation")}</a>
-                  <a href={"/faucet/"}>{t("menu.developers.faucet")}</a>
-                  <a href={"/tools/"}>Bithomp tools</a>
-                </>
-              }
-              <a href="https://docs.bithomp.com">{t("menu.developers.api")}</a>
-              <Link href="/admin">{t("menu.developers.api-admin")}</Link>
-              <a href="https://github.com/Bithomp">Github</a>
-              <Link href="/eaas">{t("menu.business.eaas")}</Link>
-              <Link href="/build-unl">{t("menu.business.build-unl")}</Link>
-            </div>
-          </div>
+          <MenuDropDown id="dropdown6" title={t("menu.developers.developers")}>
+            {devNet &&
+              <>
+                <a href={"/create/"}>{t("menu.developers.account-generation")}</a>
+                <a href={"/faucet/"}>{t("menu.developers.faucet")}</a>
+                <a href={"/tools/"}>Bithomp tools</a>
+              </>
+            }
+            <a href="https://docs.bithomp.com">{t("menu.developers.api")}</a>
+            <Link href="/admin">{t("menu.developers.api-admin")}</Link>
+            <a href="https://github.com/Bithomp">Github</a>
+            <Link href="/eaas">{t("menu.business.eaas")}</Link>
+            <Link href="/build-unl">{t("menu.business.build-unl")}</Link>
+          </MenuDropDown>
 
-          <div className="menu-dropdown" id="dropdown7">
-            <div className="menu-dropdown-button">{t("menu.networks")}</div>
-            <div className="menu-dropdown-content" style={{ display: hoverStates["dropdown7"] ? 'block' : 'none' }}>
-              {network !== 'xahau' && <a href="https://xahauexplorer.com">XAHAU Mainnet</a>}
-              {network !== 'xahau-testnet' && <a href="https://test.xahauexplorer.com">XAHAU Testnet</a>}
-              {network !== 'mainnet' && <a href="https://bithomp.com">XRPL Mainnet</a>}
-              {network !== 'testnet' && <a href="https://test.bithomp.com">XRPL Testnet</a>}
-              {network !== 'devnet' && <a href="https://dev.bithomp.com">XRPL Devnet</a>}
-            </div>
-          </div>
-        </div>
+          <MenuDropDown id="dropdown7" title={t("menu.networks")}>
+            {network !== 'xahau' && <a href="https://xahauexplorer.com">XAHAU Mainnet</a>}
+            {network !== 'xahau-testnet' && <a href="https://test.xahauexplorer.com">XAHAU Testnet</a>}
+            {network !== 'mainnet' && <a href="https://bithomp.com">XRPL Mainnet</a>}
+            {network !== 'testnet' && <a href="https://test.bithomp.com">XRPL Testnet</a>}
+            {network !== 'devnet' && <a href="https://dev.bithomp.com">XRPL Devnet</a>}
+          </MenuDropDown>
+        </div >
         <div className="header-menu-right">
           {displayName ?
-            <div className="menu-dropdown" id="dropdown8">
-              <div className="menu-dropdown-button">
-                <img src={hashicon} alt="user icon" className="user-icon" />
-                {displayName}
-              </div>
-              <div className="menu-dropdown-content" style={{ display: hoverStates["dropdown8"] ? 'block' : 'none' }}>
-                <span onClick={copyToClipboard} className="link">
-                  {isCopied ? t("button.copied") : t("button.copy-my-address")}
-                </span>
-                <Link href={"/nfts/" + address}>{t("signin.actions.my-nfts")}</Link>
+            <MenuDropDown
+              id="dropdown8"
+              title={
+                <>
+                  <img src={hashicon} alt="user icon" className="user-icon" />
+                  {displayName}
+                </>
+              }
+            >
+              <span onClick={copyToClipboard} className="link">
+                {isCopied ? t("button.copied") : t("button.copy-my-address")}
+              </span>
+              <Link href={"/nfts/" + address}>{t("signin.actions.my-nfts")}</Link>
 
-                {/* Hide My NFT Offers for XAHAU while they are not ready yet */}
-                {!xahauNetwork &&
-                  <Link href={"/nft-offers/" + address}>{t("signin.actions.my-nft-offers")}</Link>
-                }
+              {/* Hide My NFT Offers for XAHAU while they are not ready yet */}
+              {!xahauNetwork &&
+                <Link href={"/nft-offers/" + address}>{t("signin.actions.my-nft-offers")}</Link>
+              }
 
-                {xummUserToken && <a href={"/explorer/" + address + "?hw=xumm&xummtoken=" + xummUserToken}>{t("signin.actions.view")}</a>}
-                {!username && <Link href={"/username?address=" + address}>{t("menu.usernames")}</Link>}
+              {xummUserToken && <a href={"/explorer/" + address + "?hw=xumm&xummtoken=" + xummUserToken}>{t("signin.actions.view")}</a>}
+              {!username && <Link href={"/username?address=" + address}>{t("menu.usernames")}</Link>}
 
-                {/* Hide Send XRP for XAHAU while they are not ready yet */}
-                {!xahauNetwork &&
-                  <>
-                    {xummUserToken && <a href={"/explorer/" + address + "?hw=xumm&xummtoken=" + xummUserToken + "&action=send"}>{t("signin.actions.send")}</a>}
-                  </>
-                }
+              {/* Hide Send XRP for XAHAU while they are not ready yet */}
+              {!xahauNetwork &&
+                <>
+                  {xummUserToken && <a href={"/explorer/" + address + "?hw=xumm&xummtoken=" + xummUserToken + "&action=send"}>{t("signin.actions.send")}</a>}
+                </>
+              }
 
-                <span onClick={signOut} className="link">{t("signin.signout")}</span>
-              </div>
-            </div>
+              <span onClick={signOut} className="link">{t("signin.signout")}</span>
+            </MenuDropDown>
             :
             <span onClick={() => { setSignRequest(true) }} className="header-signin-link link">{t("signin.signin")}</span>
           }
