f0ca558b83bf8ca11702db536e088a67917fb348
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-07-09 14:12:55 +0400

    reddit link update

--- components/Layout/SocialIcons.js
+++ components/Layout/SocialIcons.js
@@ -9,7 +9,7 @@ export default function SocialIcons() {
       <a href="https://youtube.com/@bithomp"><img src="/images/youtube.svg" className="footer-social-icon" alt="youtube" /></a>
       <a href="https://linkedin.com/company/bithomp"><img src="/images/linkedin.svg" className="footer-social-icon" alt="linkedin" /></a>
       <a href="https://www.instagram.com/bithomp/"><img src="/images/instagram.svg" className="footer-social-icon" alt="instagram" /></a>
-      <a href="https://www.reddit.com/user/bithomp/"><img src="/images/reddit.svg" className="footer-social-icon" alt="reddit" /></a>
+      <a href="https://www.reddit.com/r/bithomp/"><img src="/images/reddit.svg" className="footer-social-icon" alt="reddit" /></a>
       <a href="https://github.com/Bithomp"><img src="/images/github.svg" className="footer-social-icon" alt="github" /></a>
     </>
   )
