1aa88e8eaad48ef496aad59a09a22d2b27fac3a5
Ivan_Vierin <ivan@huncwot.com>
2024-07-10 21:40:32 +0200

    fix: translate popup + different zindex

--- components/UI/AgeCheck.js
+++ components/UI/AgeCheck.js
@@ -1,6 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useTranslation } from "react-i18next";
 
 export default function AgeCheck() {
+  const { t } = useTranslation(['common', 'popups']);
   const [isShown, setIsShown] = useState(true)
 
   const confirmAgeClick = () => {
@@ -8,19 +10,22 @@ export default function AgeCheck() {
     setIsShown(false)
   }
 
+  useEffect(() => {
+    const content = document.querySelector('.content-text');
+    content && (content.style.zIndex = isShown ? 'auto' : '1');
+  }, [isShown])
+
   return <>
     <div className={`age-check${isShown ? ' is-visible' : ''}`}>
       <div className="age-check__header">
         <h3>18+</h3>
-        <p className="age-check__subtitle">Explicit content</p>
+        <p className="age-check__subtitle">{t("popup.age-verification.subtitle")}</p>
       </div>
-      <p className="age-check__text">
-        This will enable explicit and sensitive content. You must be of the legal age to view this.
-      </p>
+      <p className="age-check__text">{t("popup.age-verification.text")}</p>
       <br />
       <div className="age-check__btns">
-        <button className="age-check__btn" onClick={confirmAgeClick}>Yes, I'm over 18</button>
-        <button className="age-check__btn" onClick={() => setIsShown(false)}>No, I'm not</button>
+        <button className="age-check__btn" onClick={confirmAgeClick}>{t("popup.age-verification.button-true")}</button>
+        <button className="age-check__btn" onClick={() => setIsShown(false)}>{t("popup.age-verification.button-false")}</button>
       </div>
     </div>
     <div className="age-check-overlay"></div>
