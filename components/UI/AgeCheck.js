f3d794c6987813f05036397c5195f3c29e410ce6
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-07-11 14:14:31 +0400

    ageCheck: remove overlay

--- components/UI/AgeCheck.js
+++ components/UI/AgeCheck.js
@@ -8,27 +8,24 @@ export default function AgeCheck({ setShowAgeCheck }) {
     setShowAgeCheck(false)
   }
 
-  return <>
-    <div className="age-check">
-      <div className="age-check__header">
-        <h3>18+</h3>
-        <p className="age-check__subtitle">
-          {t("age-check.subtitle", { ns: "popups" })}
-        </p>
-      </div>
-      <p className="age-check__text">
-        {t("age-check.text", { ns: "popups" })}
+  return <div className="age-check">
+    <div className="age-check__header">
+      <h3>18+</h3>
+      <p className="age-check__subtitle">
+        {t("age-check.subtitle", { ns: "popups" })}
       </p>
-      <br />
-      <div className="age-check__btns">
-        <button className="age-check__btn" onClick={confirmAgeClick}>
-          {t("age-check.button-true", { ns: "popups" })}
-        </button>
-        <button className="age-check__btn" onClick={() => setShowAgeCheck(false)}>
-          {t("age-check.button-false", { ns: "popups" })}
-        </button>
-      </div>
     </div>
-    <div className="age-check-overlay"></div>
-  </>
+    <p className="age-check__text">
+      {t("age-check.text", { ns: "popups" })}
+    </p>
+    <br />
+    <div className="age-check__btns">
+      <button className="age-check__btn" onClick={confirmAgeClick}>
+        {t("age-check.button-true", { ns: "popups" })}
+      </button>
+      <button className="age-check__btn" onClick={() => setShowAgeCheck(false)}>
+        {t("age-check.button-false", { ns: "popups" })}
+      </button>
+    </div>
+  </div>
 }
