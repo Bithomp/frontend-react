aed73e839db284cb2301ea362391704820d917a9
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-08-14 11:59:47 +0200

    prettify and pro addresses history link fix

--- pages/admin/pro/history.js
+++ pages/admin/pro/history.js
@@ -16,7 +16,7 @@ export const getServerSideProps = async (context) => {
   const { address } = query
   return {
     props: {
-      queryAddress: address || "",
+      queryAddress: address || '',
       isSsrMobile: getIsSsrMobile(context),
       ...(await serverSideTranslations(locale, ['common', 'admin']))
     }
@@ -28,20 +28,20 @@ export default function History({ account, setAccount, queryAddress }) {
   const width = useWidth()
 
   const { t } = useTranslation(['common', 'admin'])
-  const [errorMessage, setErrorMessage] = useState("")
+  const [errorMessage, setErrorMessage] = useState('')
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(false)
 
   const getProAddressHistory = async () => {
     setLoading(true)
-    const response = await axiosAdmin.delete('user/address/' + queryAddress + '/activities').catch(error => {
+    const response = await axiosAdmin.get('user/address/' + queryAddress + '/activities').catch((error) => {
       setLoading(false)
-      if (error.response?.data?.error === "errors.token.required") {
+      if (error.response?.data?.error === 'errors.token.required') {
         onLogOut()
         return
       }
-      if (error && error.message !== "canceled") {
-        setErrorMessage(t(error.response?.data?.error || "error." + error.message))
+      if (error && error.message !== 'canceled') {
+        setErrorMessage(t(error.response?.data?.error || 'error.' + error.message))
       }
     })
     setLoading(false)
@@ -59,8 +59,8 @@ export default function History({ account, setAccount, queryAddress }) {
     if (!sessionToken) {
       router.push('/admin')
     } else {
-      axiosAdmin.defaults.headers.common['Authorization'] = "Bearer " + sessionToken
-      setErrorMessage("")
+      axiosAdmin.defaults.headers.common['Authorization'] = 'Bearer ' + sessionToken
+      setErrorMessage('')
     }
     getProAddressHistory()
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -69,66 +69,58 @@ export default function History({ account, setAccount, queryAddress }) {
   const onLogOut = () => {
     localStorage.removeItem('sessionToken')
     setAccount({ ...account, pro: null })
-    setErrorMessage("")
+    setErrorMessage('')
   }
 
-  return <>
-    <SEO title="Pro address: history" />
-    <div className="page-admin content-center">
-      <h1 className='center'>
-        Pro address balances history
-      </h1>
+  return (
+    <>
+      <SEO title="Pro address: history" />
+      <div className="page-admin content-center">
+        <h1 className="center">Pro address balances history</h1>
 
-      <AdminTabs name="mainTabs" tab="pro" />
+        <AdminTabs name="mainTabs" tab="pro" />
 
-      <h4 className='center'>Balances history</h4>
+        <h4 className="center">Balances history</h4>
 
-      {(!width || width > 750) ?
-        <table className='table-large'>
-          <thead>
-            <tr>
-              <th className='center'>#</th>
-              <th className='left'>Address</th>
-              <th className='right'>Private name</th>
-              <th>Verified at</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.length > 0 ?
-              <>
-                {
-                  data.map((address, i) =>
+        {!width || width > 750 ? (
+          <table className="table-large">
+            <thead>
+              <tr>
+                <th className="center">#</th>
+                <th className="left">Address</th>
+                <th className="right">Private name</th>
+                <th>Verified at</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.length > 0 ? (
+                <>
+                  {data.map((address, i) => (
                     <tr key={i}>
                       <td className="center">{i + 1}</td>
-                      <td className='left'>{address.address}</td>
-                      <td className='right'>{address.name}</td>
+                      <td className="left">{address.address}</td>
+                      <td className="right">{address.name}</td>
                       <td>{fullDateAndTime(address.createdAt)}</td>
                     </tr>
-                  )
-                }
-              </>
-              :
-              <tr>
-                <td colSpan="100" className='center'>
-                  {loading ?
-                    "Loading data..."
-                    :
-                    "You do not have verified addresses yet."
-                  }
-                </td>
-              </tr>
-            }
-          </tbody>
-        </table>
-        :
-        <table className='table-mobile'>
-          <tbody>
-            {data?.length > 0 ?
-              <>
-                {
-                  data.map((address, i) =>
+                  ))}
+                </>
+              ) : (
+                <tr>
+                  <td colSpan="100" className="center">
+                    {loading ? 'Loading data...' : 'You do not have verified addresses yet.'}
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        ) : (
+          <table className="table-mobile">
+            <tbody>
+              {data?.length > 0 ? (
+                <>
+                  {data.map((address, i) => (
                     <tr key={i}>
-                      <td style={{ padding: "5px" }}>#{i + 1}</td>
+                      <td style={{ padding: '5px' }}>#{i + 1}</td>
                       <td>
                         <p>
                           Address: <b>{address.address}</b>
@@ -136,50 +128,40 @@ export default function History({ account, setAccount, queryAddress }) {
                         <p>
                           Name: <b>{address.name}</b>
                         </p>
-                        <p>
-                          Verified at: {fullDateAndTime(address.createdAt)}
-                        </p>
+                        <p>Verified at: {fullDateAndTime(address.createdAt)}</p>
                       </td>
                     </tr>
-                  )
-                }
-              </>
-              :
-              <tr>
-                <td colSpan="100" className='center'>
-                  {loading ?
-                    "Loading data..."
-                    :
-                    "You do not have verified addresses yet."
-                  }
-                </td>
-              </tr>
-            }
-          </tbody>
-        </table>
-      }
-      <br /><br />
-      <div style={{ textAlign: "left" }}>
-        In order to use PRO functionality for your accounts, you would need to verify them first.
-        <br /><br />
-        <div>
-          - Get your personal historical transaction's extracts and statistics.
-          <br />
-          - Auto cancelation of expired NFT offers
-          <br />
-          - Auto execution of time based escrows
-        </div>
-        {width > 851 && <br />}
-        <br /><br />
-      </div>
-      <br />
-      {errorMessage ?
-        <div className='center orange bold'>
-          {errorMessage}
-        </div>
-        :
+                  ))}
+                </>
+              ) : (
+                <tr>
+                  <td colSpan="100" className="center">
+                    {loading ? 'Loading data...' : 'You do not have verified addresses yet.'}
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        )}
         <br />
-      }
-    </div>
-  </>
+        <br />
+        <div style={{ textAlign: 'left' }}>
+          In order to use PRO functionality for your accounts, you would need to verify them first.
+          <br />
+          <br />
+          <div>
+            - Get your personal historical transaction's extracts and statistics.
+            <br />
+            - Auto cancelation of expired NFT offers
+            <br />- Auto execution of time based escrows
+          </div>
+          {width > 851 && <br />}
+          <br />
+          <br />
+        </div>
+        <br />
+        {errorMessage ? <div className="center orange bold">{errorMessage}</div> : <br />}
+      </div>
+    </>
+  )
 }
