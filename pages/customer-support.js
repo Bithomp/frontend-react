4da66298cce62cfbb5cadcc2fe189d5394bd2cd1
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-08-31 18:57:38 +0200

    New support page

--- pages/customer-support.js
+++ pages/customer-support.js
@@ -6,16 +6,17 @@ import axios from 'axios'
 
 import SocialIcons from '../components/Layout/SocialIcons'
 import SEO from '../components/SEO'
-import { nativeCurrency } from '../utils'
+import { nativeCurrency, explorerName } from '../utils'
 import { getIsSsrMobile } from '../utils/mobile'
 import { amountFormat } from '../utils/format'
+import Link from 'next/link'
 
 export async function getServerSideProps(context) {
   const { locale } = context
   return {
     props: {
       isSsrMobile: getIsSsrMobile(context),
-      ...(await serverSideTranslations(locale, ['common', 'customer-support'])),
+      ...(await serverSideTranslations(locale, ['common', 'customer-support']))
     }
   }
 }
@@ -34,46 +35,100 @@ export default function Contact() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  return <>
-    <SEO title={t("menu.customer-support")} />
-    <div className="content-text">
-      <h1>{t("menu.customer-support")}</h1>
-      <p>
-        <Trans ns="customer-support" i18nKey="text0">
-          Bithomp is an explorer of the <u>public</u> {{ nativeCurrency }} Ledger.
-        </Trans>
-      </p>
-      <p>{t("no-help", { ns: "customer-support" })}</p>
-      <ol>
-        <li>
-          <Trans ns="customer-support" i18nKey="text1">
-            If you funds were stolen or if you became a victim of fraud/scam you can report it <a href="https://xrplorer.com/forensics/help">here</a>.
+  return (
+    <>
+      <SEO title={t('header', { ns: 'customer-support' })} />
+      <div className="content-text">
+        <h1>{t('header', { ns: 'customer-support' })}</h1>
+        <p>{t('read-carefully', { ns: 'customer-support' }).toUpperCase()}</p>
+        <p>
+          <Trans ns="customer-support" i18nKey="is-explorer">
+            Our web-site operates as an explorer of <strong>PUBLICLY</strong> available information on the{' '}
+            {{ explorerName }} Ledger.
           </Trans>
-        </li>
-        <li>{t("status-failed", { ns: "customer-support" })}</li>
-        <li>{t("status-success", { ns: "customer-support" })}</li>
-        <li>{t("wrong-tag", { ns: "customer-support" })}</li>
-        <li>
-          <Trans ns="customer-support" i18nKey="text2">
-            If you are missing {{ baseReserve: amountFormat(networkInfo?.reserveBase) }} in your wallet, please read about the <a href="https://xrpl.org/reserves.html">base reserve</a>.
+          <br />
+          <Trans ns="customer-support" i18nKey="no-wallet" />
+          <br />
+          <Trans ns="customer-support" i18nKey="primary-function">
+            Our <strong>primary function</strong> is to provide information about transactions and accounts on the{' '}
+            {{ explorerName }} in a user-friendly manner. The information we provide is not proprietary or secret; it is
+            available to anyone with access to the blockchain.
           </Trans>
-        </li>
-        <li>{t("paper-wallet", { ns: "customer-support", nativeCurrency })}</li>
-        <li>
-          <Trans ns="customer-support" i18nKey="text3">
-            If you have a <b>partnership or marketing</b> proposals then you can contact us by email: <Mailto email='partner@bithomp.com' headers={{ subject: 'Bithomp partner' }} />. You can also contact us with questions about the bithomp username registration or bithomp transaction explorer.
+        </p>
+        <h3>{t('what-we-cannot-do', { ns: 'customer-support' })}</h3>
+        <ul>
+          <li>
+            <Trans ns="customer-support" i18nKey="freeze-accounts" values={{ explorerName }} />
+          </li>
+          <li>
+            <Trans ns="customer-support" i18nKey="reverse-transactions" />
+          </li>
+          <li>
+            <Trans ns="customer-support" i18nKey="modify-tags" />
+          </li>
+          <li>
+            <Trans ns="customer-support" i18nKey="conduct-investigations" />
+          </li>
+          <li>
+            <Trans ns="customer-support" i18nKey="recover-funds" values={{ nativeCurrency }} />
+          </li>
+        </ul>
+        <h3>{t('what-to-do', { ns: 'customer-support' })}</h3>
+        <ol>
+          <li>
+            <Trans ns="customer-support" i18nKey="stolen-funds">
+              <strong>Stolen Funds or Fraud/Scam Victim</strong>: If your funds were stolen or you were scammed, you can
+              report the incident <a href="https://xrplorer.com/forensics/help">here</a>.
+            </Trans>
+          </li>
+          <li>
+            <Trans ns="customer-support" i18nKey="failed-transaction" />
+          </li>
+          <li>
+            <Trans ns="customer-support" i18nKey="successful-transaction" />
+          </li>
+          <li>
+            <Trans ns="customer-support" i18nKey="destination-tag" />
+          </li>
+          <li>
+            <Trans ns="customer-support" i18nKey="missing-base-reserve">
+              <strong>Missing {{ baseReserve: amountFormat(networkInfo?.reserveBase) }} in Your Wallet</strong>: The
+              missing {{ baseReserve: amountFormat(networkInfo?.reserveBase) }} is due to the {{ nativeCurrency }}{' '}
+              Ledger's base reserve requirement. Please read more about the{' '}
+              <a href="https://xrpl.org/reserves.html">base reserve</a>.
+            </Trans>
+          </li>
+          <li>
+            <Trans ns="customer-support" i18nKey="sending-from-paper-wallet" values={{ nativeCurrency }} />
+          </li>
+        </ol>
+        <p>
+          <Trans ns="customer-support" i18nKey="note" />
+        </p>
+        <p>
+          <b>{t('inquiries', { ns: 'customer-support' })}</b>
+          <br />
+          <Trans ns="customer-support" i18nKey="for-partnership">
+            For any partnership or marketing proposals:{' '}
+            <Mailto email="partner@bithomp.com" headers={{ subject: 'Bithomp partner' }} />
           </Trans>
-        </li>
-      </ol>
-      <p>{t("no-answer", { ns: "customer-support" })}</p>
-      <h2>{t("submit-info", { ns: "customer-support" })}</h2>
-      <p>
-        <Trans ns="customer-support" i18nKey="text4">
-          If you have a public service and you want your addresses to be recognised on Bithomp submit your information <a href="https://bithomp.com/explorer/submit.html">here</a>.
-        </Trans>
-      </p>
-      <h2>{t("follow-us", { ns: "customer-support" })}</h2>
-      <SocialIcons />
-    </div>
-  </>
-};
+          <br />
+          <Trans ns="customer-support" i18nKey="for-questions">
+            For any questions about the username registration, transaction explorer or Bithomp Pro:{' '}
+            <Mailto email="inquiries@bithomp.com" headers={{ subject: 'Bithomp usage question' }} />
+          </Trans>
+        </p>
+
+        <h3>{t('submit-info', { ns: 'customer-support' })}</h3>
+        <p>
+          <Trans ns="customer-support" i18nKey="submit-info-here">
+            If you have a public service and you want your addresses to be recognised, submit your information{' '}
+            <Link href="/submit-account-information">here</Link>.
+          </Trans>
+        </p>
+        <h3>{t('follow-us', { ns: 'customer-support' })}</h3>
+        <SocialIcons />
+      </div>
+    </>
+  )
+}
