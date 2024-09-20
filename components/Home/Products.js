f7de7a99e8e6f3c7658bfc1f2858d244ba327a32
Vierin <ivanvierin@example.com>
2024-09-20 10:03:03 +0200

    delete: hover + fix links + product in row from 1500px

--- components/Home/Products.js
+++ components/Home/Products.js
@@ -11,11 +11,26 @@ export default function Products() {
       type: 'nft',
       title: t('titles.nft', { ns: 'products' }),
       list: [
-        t('items.search-name', { ns: 'products' }),
-        t('items.check', { ns: 'products' }),
-        t('items.your-nft', { ns: 'products' }),
-        t('items.buy-sell', { ns: 'products' }),
-        t('items.manage-nft', { ns: 'products' })
+        {
+          link: '/',
+          text: t('items.search-name', { ns: 'products' }),
+        },
+        {
+          link: '/',
+          text: t('items.check', { ns: 'products' }),
+        },
+        {
+          link: '/',
+          text: t('items.your-nft', { ns: 'products' }),
+        },
+        {
+          link: '/',
+          text: t('items.buy-sell', { ns: 'products' }),
+        },
+        {
+          link: '/',
+          text: t('items.manage-nft', { ns: 'products' }),
+        }
       ],
       bg: '/images/products/product-bg-1.png'
     },
@@ -23,10 +38,22 @@ export default function Products() {
       type: 'api',
       title: t('titles.api', { ns: 'products' }),
       list: [
-        t('items.registration', { ns: 'products' }),
-        t('items.balance', { ns: 'products' }),
-        t('items.transaction-status', { ns: 'products' }),
-        t('items.statistics', { ns: 'products' })
+        {
+          link: '/',
+          text: t('items.registration', { ns: 'products' }),
+        },
+        {
+          link: '/',
+          text: t('items.balance', { ns: 'products' }),
+        },
+        {
+          link: '/',
+          text: t('items.transaction-status', { ns: 'products' }),
+        },
+        {
+          link: '/',
+          text: t('items.statistics', { ns: 'products' }),
+        }
       ],
       bg: '/images/products/product-bg-2.png',
       gradient: '/images/products/gradient-3.svg'
@@ -34,7 +61,16 @@ export default function Products() {
     {
       type: 'xrpl',
       title: t('titles.xrpl', { ns: 'products' }),
-      list: [t('items.domain', { ns: 'products' }), t('items.mint-nft', { ns: 'products' })],
+      list: [
+        {
+          link: '/',
+          text: t('items.domain', { ns: 'products' }),
+        },
+        {
+          link: '/',
+          text: t('items.mint-nft', { ns: 'products' }),
+        }
+      ],
       bg: '/images/products/product-bg-3.png',
       gradient: '/images/products/gradient-1.svg'
     },
@@ -42,10 +78,22 @@ export default function Products() {
       type: 'services',
       title: t('titles.services', { ns: 'products' }),
       list: [
-        t('items.nft-bots', { ns: 'products' }),
-        t('items.notifications', { ns: 'products' }),
-        t('items.alerts', { ns: 'products' }),
-        t('items.usernames', { ns: 'products' })
+        {
+          link: '/',
+          text: t('items.nft-bots', { ns: 'products' })
+        },
+        {
+          link: '/',
+          text: t('items.notifications', { ns: 'products' })
+        },
+        {
+          link: '/',
+          text: t('items.alerts', { ns: 'products' })
+        },
+        {
+          link: '/',
+          text: t('items.usernames', { ns: 'products' })
+        }
       ],
       bg: '/images/products/product-bg-4.png',
       gradient: '/images/products/gradient-2.svg'
@@ -57,19 +105,21 @@ export default function Products() {
       {products.map((product, i) => {
         return (
           <div key={i} className={'product product--' + product.type}>
-            <Link className="product__wrap" href="/">
+            <div className="product__wrap">
               <h2>{product.title}</h2>
               <ul>
                 {product.list.map((item, index) => {
                   return (
                     <li key={index}>
-                      <img alt="bithomp logo" src={logo} />
-                      <span>{item}</span>
+                      <Link href={item.link}>
+                        <img alt="bithomp logo" src={logo} />
+                        <span>{item.text}</span>
+                      </Link>
                     </li>
                   )
                 })}
               </ul>
-            </Link>
+            </div>
             <div className="product__bg">
               <img alt="bithomp logo" src={product.bg} />
             </div>
