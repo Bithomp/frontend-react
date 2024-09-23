cbb4eaf98c0a8305e51f0b8c6ef02921d6454410
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-09-23 12:51:54 +0200

    Faucet: keep the params when switching the network

--- components/Tabs/FaucetTabs.js
+++ components/Tabs/FaucetTabs.js
@@ -1,8 +1,11 @@
 import Tabs from '.'
 
 import { networks, network } from '../../utils'
+import { useRouter } from 'next/router'
 
 export default function FaucetTabs() {
+  const router = useRouter()
+
   let apiTabs = [
     { value: 'testnet', label: networks['testnet'].explorerName },
     { value: 'devnet', label: networks['devnet'].explorerName },
@@ -11,7 +14,7 @@ export default function FaucetTabs() {
 
   const changePage = (tab) => {
     const server = networks[tab].server
-    location.href = server + '/faucet'
+    location.href = server + router.asPath
   }
 
   return <Tabs tabList={apiTabs} tab={network} setTab={changePage} name="apiTabs" />
