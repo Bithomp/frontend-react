c74fd6f7d948a83b98001aac3617e6d5b2a3e2f4
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-10-30 09:23:53 +0100

    ButtonScrollTop - split css

--- components/Layout/ButtonScrollTop.js
+++ components/Layout/ButtonScrollTop.js
@@ -1,37 +1,47 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from 'react'
+import { buttonArrowScroll } from '../../styles/components/buttonScrollTop.module.scss'
 
-const arrowTop = "/images/arrow-top.svg"
+const arrowTop = '/images/arrow-top.svg'
 
 export default function ButtonScrollTop({ footer }) {
+  const [buttonState, setButtonState] = useState({ isShown: false, isFixedButton: false })
 
-    const [buttonState, setButtonState] = useState({ isShown: false, isFixedButton: false });
+  const handleScroll = useCallback(() => {
+    const scrollPosition = window.pageYOffset + window.innerHeight
+    const isShown = window.pageYOffset !== 0
+    const isPhone = window.matchMedia('(max-width: 768px)').matches
 
-    const handleScroll = useCallback(() => {
-        const scrollPosition = window.pageYOffset + window.innerHeight;
-        const isShown = window.pageYOffset !== 0;
-        const isPhone = window.matchMedia("(max-width: 768px)").matches;
+    const isFixedButton =
+      scrollPosition > (!isPhone ? footer.current.offsetTop : footer.current.offsetTop + footer.current.offsetHeight)
 
-        const isFixedButton = scrollPosition > (!isPhone ? footer.current.offsetTop : footer.current.offsetTop + footer.current.offsetHeight);
+    setButtonState({ isShown, isFixedButton })
+  }, [footer])
 
-        setButtonState({ isShown, isFixedButton });
-    }, [footer]);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  const scrollTop = useCallback(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [])
 
-    const scrollTop = useCallback(() => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }, []);
-
-    return (
-        <button className={'button-arrow-scroll' + (buttonState.isShown ? ' is-shown' : '') + (buttonState.isFixedButton ? ' is-fixed' : '')} onClick={scrollTop}>
-            <img src={arrowTop} className="arrow-top" alt="arrow to scroll top" />
-        </button>
-    )
+  return (
+    <div className={buttonArrowScroll}>
+      <button
+        className={
+          'button-arrow-scroll' +
+          (buttonState.isShown ? ' is-shown' : '') +
+          (buttonState.isFixedButton ? ' is-fixed' : '')
+        }
+        onClick={scrollTop}
+      >
+        <img src={arrowTop} className="arrow-top" alt="arrow to scroll top" />
+      </button>
+    </div>
+  )
 }
