d7cf3676fc5b0df46b97ad2153db05527cd160b1
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-11-01 13:08:12 +0100

    social icons: area-labels

--- components/Layout/SocialIcons.js
+++ components/Layout/SocialIcons.js
@@ -14,40 +14,40 @@ import { FaMedium } from 'react-icons/fa6'
 export default function SocialIcons() {
   return (
     <div className="social-icons">
-      <a href={'https://x.com/' + (xahauNetwork ? 'XahauExplorer' : 'xrplexplorer')}>
+      <a href={'https://x.com/' + (xahauNetwork ? 'XahauExplorer' : 'xrplexplorer')} aria-label="X link">
         <FaXTwitter />
       </a>
-      <a href="https://discord.gg/ZahGJ29WEs">
+      <a href="https://discord.gg/ZahGJ29WEs" area-label="Discord link">
         <FaDiscord />
       </a>
-      <a href="https://tiktok.com/@bithomp">
+      <a href="https://tiktok.com/@bithomp" area-label="Tiktok link">
         <FaTiktok />
       </a>
-      <a href="https://youtube.com/@bithomp">
+      <a href="https://youtube.com/@bithomp" area-label="Youtube link">
         <FaYoutube />
       </a>
-      <a href="https://linkedin.com/company/bithomp">
+      <a href="https://linkedin.com/company/bithomp" area-label="Linkedin link">
         <FaLinkedin />
       </a>
-      <a href="https://www.instagram.com/bithomp/">
+      <a href="https://www.instagram.com/bithomp/" area-label="Instagram link">
         <FaInstagram />
       </a>
       {/*
-      <a href="https://t.me/bithomp">
+      <a href="https://t.me/bithomp" area-label="Telegram link">
         <FaTelegram />
       </a>
       */}
       {!xahauNetwork && (
-        <a href="https://www.facebook.com/xrpexplorer/">
+        <a href="https://www.facebook.com/xrpexplorer/" area-label="Facebook link">
           <FaFacebook />
         </a>
       )}
       {!xahauNetwork && (
-        <a href="https://medium.com/@xrplexplorer">
+        <a href="https://medium.com/@xrplexplorer" area-label="Medium link">
           <FaMedium />
         </a>
       )}
-      <a href="https://github.com/Bithomp">
+      <a href="https://github.com/Bithomp" area-label="Github link">
         <FaGithub />
       </a>
     </div>
