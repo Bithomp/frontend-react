68b4411080e3e102171e6aa96cc0a08c70411805
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-11-06 17:07:17 +0100

    domains translation fixes

--- pages/genesis.js
+++ pages/genesis.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useTranslation, Trans } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-import { useIsMobile, getIsSsrMobile } from "../utils/mobile"
+import { useIsMobile, getIsSsrMobile } from '../utils/mobile'
 import { dateFormat, addressUsernameOrServiceLink, amountFormat } from '../utils/format'
 import { network } from '../utils'
 
@@ -12,7 +12,7 @@ export async function getServerSideProps(context) {
   return {
     props: {
       isSsrMobile: getIsSsrMobile(context),
-      ...(await serverSideTranslations(locale, ['common'])),
+      ...(await serverSideTranslations(locale, ['common']))
     }
   }
 }
@@ -51,114 +51,138 @@ export default function Genesis() {
               "balance": 20.009958
             },
       */
-      const response = await axios('v2/genesis');
-      setData(response.data);
+      const response = await axios('v2/genesis')
+      setData(response.data)
     }
-    fetchData();
-  }, [setData]);
+    fetchData()
+  }, [setData])
 
-  const timestampFormatParams = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+  const timestampFormatParams = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' }
 
-  return <>
-    <SEO title={t("menu.network.genesis")} />
-    <div className="page-genesis content-text">
-      <h1 className="center">{t("menu.network.genesis")}</h1>
+  return (
+    <>
+      <SEO title={t('menu.network.genesis')} />
+      <div className="page-genesis content-text">
+        <h1 className="center">{t('menu.network.genesis')}</h1>
 
-      {/* TODO add description for other networks like in ledger/[[...ledgerindex]].js */}
-      {network === "mainnet" &&
-        <div className='flex'>
-          <div className="grey-box">
-            <Trans i18nKey="genesis.text0">
-              The ledger <b>32570</b> is the earliest ledger available, approximately the first week of XRPL history,
-              <a
-                href="https://web.archive.org/web/20171211225452/https://forum.ripple.com/viewtopic.php?f=2&t=3613"
-                rel="noreferrer"
-                target="_blank"
-              >
-                ledgers 1 through 32569 were lost due to a mishap in 2012
-              </a>.
-            </Trans>
-            <br /><br />
-            {t("genesis.ledger-continue")}
+        {/* TODO add description for other networks like in ledger/[[...ledgerindex]].js */}
+        {network === 'mainnet' && (
+          <div className="flex">
+            <div className="grey-box">
+              <Trans i18nKey="genesis.text0">
+                The ledger <b>32570</b> is the earliest ledger available, approximately the first week of XRPL history,
+                <a
+                  href="https://web.archive.org/web/20171211225452/https://forum.ripple.com/viewtopic.php?f=2&t=3613"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  ledgers 1 through 32569 were lost due to a mishap in 2012
+                </a>
+                .
+              </Trans>
+              <br />
+              <br />
+              {t('genesis.ledger-continue')}
+            </div>
+            <div className="grey-box">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>{t('genesis.ledger-index')}</td>
+                    <td>32570</td>
+                  </tr>
+                  <tr>
+                    <td>{t('genesis.account-count')}</td>
+                    <td>136</td>
+                  </tr>
+                  <tr>
+                    <td>{t('genesis.inception')}</td>
+                    <td>{rendered && dateFormat('2013-01-01T03:21:00Z', timestampFormatParams, { type: 'ISO' })}</td>
+                  </tr>
+                  <tr>
+                    <td>{t('table.balance')}</td>
+                    <td>{amountFormat(99999999999996320, { minFractionDigits: 6 })}</td>
+                  </tr>
+                  <tr>
+                    <td colSpan="2">
+                      <hr />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>{t('genesis.balance-update')}</td>
+                    <td>{rendered && dateFormat(data.balance_update, timestampFormatParams)}</td>
+                  </tr>
+                  <tr>
+                    <td>{t('table.balance')}</td>
+                    <td>{data.balance_all && amountFormat(data.balance_all, { minFractionDigits: 6 })}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-          <div className="grey-box">
-            <table>
-              <tbody>
-                <tr><td>{t("genesis.ledger-index")}</td><td>32570</td></tr>
-                <tr><td>{t("genesis.account-count")}</td><td>136</td></tr>
-                <tr>
-                  <td>{t("genesis.inception")}</td>
-                  <td>{rendered && dateFormat("2013-01-01T03:21:00Z", timestampFormatParams, { type: "ISO" })}</td>
-                </tr>
-                <tr><td>{t("table.balance")}</td><td>{amountFormat(99999999999996320, { minFractionDigits: 6 })}</td></tr>
-                <tr><td colSpan="2"><hr /></td></tr>
-                <tr>
-                  <td>{t("genesis.balance-update")}</td>
-                  <td>{rendered && dateFormat(data.balance_update, timestampFormatParams)}</td>
-                </tr>
-                <tr>
-                  <td>{t("table.balance")}</td>
-                  <td>{data.balance_all && amountFormat(data.balance_all, { minFractionDigits: 6 })}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      }
-      <br />
+        )}
+        <br />
 
-      {isMobile ?
-        <table className="table-mobile">
-          <thead>
-            <tr>
-              <th>№</th>
-              <th>{t("genesis.details")}</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.genesis?.map((account, i) => (
-              <tr key={i}>
-                <td>{account.genesis_index}</td>
-                <td>
-                  <p>
-                    {t("genesis.address")}<br />
-                    {addressUsernameOrServiceLink(account, 'address')} <CopyButton text={account.address} />
-                  </p>
-                  <p>
-                    {t("genesis.genesis-balance")}<br />
-                    {amountFormat(account.genesis_balance)}
-                  </p>
-                  <p>
-                    {t("table.balance")} {rendered && dateFormat(data.balance_update)}<br />
-                    {amountFormat(account.balance)}
-                  </p>
-                </td>
+        {isMobile ? (
+          <table className="table-mobile">
+            <thead>
+              <tr>
+                <th>№</th>
+                <th>{t('genesis.details')}</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        :
-        <table className="table-large">
-          <thead>
-            <tr>
-              <th className='center'>{t("genesis.genesis-index")}</th>
-              <th>{t("genesis.address")}</th>
-              <th className='right'>{t("genesis.genesis-balance")}</th>
-              <th className='right'>{t("table.balance")} {rendered && dateFormat(data.balance_update)}</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.genesis?.map((account, i) => (
-              <tr key={i}>
-                <td className='center'>{account.genesis_index}</td>
-                <td><CopyButton text={account.address} /> {addressUsernameOrServiceLink(account, 'address')}</td>
-                <td className='right'>{amountFormat(account.genesis_balance)}</td>
-                <td className='right'>{amountFormat(account.balance)}</td>
+            </thead>
+            <tbody>
+              {data?.genesis?.map((account, i) => (
+                <tr key={i}>
+                  <td>{account.genesis_index}</td>
+                  <td>
+                    <p>
+                      {t('genesis.address')}
+                      <br />
+                      {addressUsernameOrServiceLink(account, 'address')} <CopyButton text={account.address} />
+                    </p>
+                    <p>
+                      {t('genesis.genesis-balance')}
+                      <br />
+                      {amountFormat(account.genesis_balance)}
+                    </p>
+                    <p>
+                      {t('table.balance')} {rendered && dateFormat(data.balance_update)}
+                      <br />
+                      {amountFormat(account.balance)}
+                    </p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <table className="table-large">
+            <thead>
+              <tr>
+                <th className="center">{t('genesis.genesis-index')}</th>
+                <th>{t('genesis.address')}</th>
+                <th className="right">{t('genesis.genesis-balance')}</th>
+                <th className="right">
+                  {t('table.balance')} {rendered && dateFormat(data.balance_update)}
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      }
-    </div>
-  </>
-};
+            </thead>
+            <tbody>
+              {data?.genesis?.map((account, i) => (
+                <tr key={i}>
+                  <td className="center">{account.genesis_index}</td>
+                  <td>
+                    <CopyButton text={account.address} /> {addressUsernameOrServiceLink(account, 'address')}
+                  </td>
+                  <td className="right">{amountFormat(account.genesis_balance)}</td>
+                  <td className="right">{amountFormat(account.balance)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  )
+}
