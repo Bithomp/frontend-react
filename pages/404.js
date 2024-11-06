aa8f9b3054dfaf224fcc39d83985475b081695a1
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-11-06 16:47:25 +0100

    404 page translation fixup

--- pages/404.js
+++ pages/404.js
@@ -7,8 +7,8 @@ import SEO from '../components/SEO'
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common'])),
-    },
+      ...(await serverSideTranslations(locale, ['common']))
+    }
   }
 }
 
@@ -17,12 +17,16 @@ export default function Eror404() {
 
   return (
     <>
-      <SEO title={t("page-not-found.header")} />
+      <SEO title={t('page-not-found.header')} />
       <div className="content-text center">
-        <h1>{t("page-not-found.header")}</h1>
+        <h1>{t('page-not-found.header')}</h1>
         <p>
           <Trans i18nKey="page-not-found.text">
-            Click <Link href="/" className="bold">here</Link> to check our landing page.
+            Click{' '}
+            <Link href="/" className="bold">
+              here
+            </Link>{' '}
+            to check our landing page.
           </Trans>
         </p>
       </div>
