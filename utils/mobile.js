2fc003f117993a01d4f4af49e3c07997df36c380
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-11-23 16:42:53 +0100

    refactor

--- utils/mobile.js
+++ utils/mobile.js
@@ -1,11 +1,11 @@
-import MobileDetect from "mobile-detect"
-import { createContext, useContext } from "react"
+import MobileDetect from 'mobile-detect'
+import { createContext, useContext } from 'react'
 import { useWidth } from '.'
 
 export const IsSsrMobileContext = createContext(false)
 
 export const getIsSsrMobile = (context) => {
-  const md = new MobileDetect(context.req.headers["user-agent"])
+  const md = new MobileDetect(context.req.headers['user-agent'])
   return Boolean(md.mobile())
 }
 
@@ -14,4 +14,4 @@ export const useIsMobile = () => {
   const windowWidth = useWidth()
   const isBrowserMobile = !!windowWidth && windowWidth < 992
   return isSsrMobile || isBrowserMobile
-};
+}
