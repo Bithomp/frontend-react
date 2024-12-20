ba5bc602c9f3b2df643f68e7856de3a33f9d16dc
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2024-12-20 14:37:07 +0100

    Privacy policy: add cookie xamanBuyPro

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -25,7 +25,7 @@ export default function PrivacyPolicy() {
       <SEO title={t('menu.privacy-policy')} noindex={true} />
       <div className="content-text">
         <h1>Privacy Policy</h1>
-        <p>Last updated: November 11, 2024</p>
+        <p>Last updated: December 20, 2024</p>
         <p>
           This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your
           information when You use the Service and tells You about Your privacy rights and how the law protects You.
@@ -278,6 +278,9 @@ export default function PrivacyPolicy() {
           </li>
           <li>
             <strong>currency</strong> - the prefered Service currency (ex: "sek").
+          </li>
+          <li>
+            <strong>xamanBuyPro</strong> - save the status of the last check of Xaman Pro limits for 10 minutes.
           </li>
           <li>
             <strong>_ga</strong> - the main cookie used by Google Analytics, enables a service to distinguish one
