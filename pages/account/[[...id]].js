40f237a030b4af49cf7c781c75645065d7027f2e
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-01-30 14:07:48 +0100

    Account: Links to other explorers

--- pages/account/[[...id]].js
+++ pages/account/[[...id]].js
@@ -1519,6 +1519,110 @@ export default function Account({
                                     )}
                                   </td>
                                 </tr>
+                                <tr>
+                                  <td>On other explorers</td>
+                                  <td>
+                                    {network === 'mainnet' && (
+                                      <>
+                                        <a href={'https://xrplexplorer.com/explorer/' + data?.address} rel="nofollow">
+                                          Bithomp
+                                        </a>{' '}
+                                        (old view) |{' '}
+                                        <a href={'https://livenet.xrpl.org/accounts/' + data?.address} rel="nofollow">
+                                          XRPL.org
+                                        </a>{' '}
+                                        |{' '}
+                                        <a href={'https://explorer.xrplf.org/' + data?.address} rel="nofollow">
+                                          XRPLF
+                                        </a>{' '}
+                                        |{' '}
+                                        <a href={'https://xrpscan.com/account/' + data?.address} rel="nofollow">
+                                          XRPScan
+                                        </a>{' '}
+                                        |{' '}
+                                        <a href={'https://xrplwin.com/account/' + data?.address} rel="nofollow">
+                                          XRPLWin
+                                        </a>{' '}
+                                        |{' '}
+                                        <a
+                                          href={'https://blockchair.com/xrp-ledger/account/' + data?.address}
+                                          rel="nofollow"
+                                        >
+                                          Blockchair
+                                        </a>{' '}
+                                        |{' '}
+                                        <a href={'https://gatehub.net/explorer/' + data?.address} rel="nofollow">
+                                          Gatehub
+                                        </a>{' '}
+                                        |{' '}
+                                        <a
+                                          href={'https://explorer.bitquery.io/ripple/address/' + data?.address}
+                                          rel="nofollow"
+                                        >
+                                          BitQuery
+                                        </a>
+                                      </>
+                                    )}
+                                    {network === 'testnet' && (
+                                      <>
+                                        <a href={'https://test.xrplexplorer.com/explorer/' + data?.address}>Bithomp</a>{' '}
+                                        (old view) |{' '}
+                                        <a href={'https://testnet.xrpl.org/accounts/' + data?.address} rel="nofollow">
+                                          XRPL.org
+                                        </a>{' '}
+                                        |{' '}
+                                        <a href={'https://explorer-testnet.xrplf.org/' + data?.address} rel="nofollow">
+                                          XRPLF
+                                        </a>
+                                      </>
+                                    )}
+                                    {network === 'devnet' && (
+                                      <>
+                                        <a href={'https://dev.xrplexplorer.com/explorer/' + data?.address}>Bithomp</a>{' '}
+                                        (old view) |{' '}
+                                        <a href={'https://devnet.xrpl.org/accounts/' + data?.address} rel="nofollow">
+                                          XRPL.org
+                                        </a>
+                                      </>
+                                    )}
+                                    {network === 'xahau' && (
+                                      <>
+                                        <a href={'https://xahauexplorer.com/explorer/' + data?.address}>
+                                          Xahau Explorer
+                                        </a>{' '}
+                                        (old view) |{' '}
+                                        <a href={'https://xahau.xrpl.org/accounts/' + data?.address} rel="nofollow">
+                                          XRPL.org
+                                        </a>{' '}
+                                        |{' '}
+                                        <a href={'https://explorer.xahau.network/' + data?.address} rel="nofollow">
+                                          XRPLF
+                                        </a>
+                                      </>
+                                    )}
+                                    {network === 'xahau-testnet' && (
+                                      <>
+                                        <a
+                                          href={'https://test.xahauexplorer.com/explorer/' + data?.address}
+                                          rel="nofollow"
+                                        >
+                                          Xahau Explorer
+                                        </a>{' '}
+                                        (old view) |
+                                        <a
+                                          href={'https://xahau-testnet.xrpl.org/accounts/' + data?.address}
+                                          rel="nofollow"
+                                        >
+                                          XRPL.org
+                                        </a>{' '}
+                                        |{' '}
+                                        <a href={'https://explorer.xahau-test.net/' + data?.address} rel="nofollow">
+                                          XRPLF
+                                        </a>
+                                      </>
+                                    )}
+                                  </td>
+                                </tr>
                               </tbody>
                             </table>
                           )}
