dfd0dceb2334b0d1d5c9a12dc436843b308f15c8
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-02-06 19:30:09 +0100

    tx: SetRegularKey

---
+++ components/Transaction/TransactionSetRegularKey.js
@@ -0,0 +1,20 @@
+import { TData, TRow } from '../TableDetails'
+
+import { TransactionCard } from './TransactionCard'
+import { AddressWithIconFilled } from '../../utils/format'
+
+export const TransactionSetRegularKey = ({ data, pageFiatRate, selectedCurrency }) => {
+  if (!data) return null
+  const { specification } = data
+
+  return (
+    <TransactionCard data={data} pageFiatRate={pageFiatRate} selectedCurrency={selectedCurrency}>
+      <TRow>
+        <TData className="bold">New Regular key:</TData>
+        <TData>
+          <AddressWithIconFilled data={specification} name="regularKey" />
+        </TData>
+      </TRow>
+    </TransactionCard>
+  )
+}
