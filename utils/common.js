a6d3314796b9796c0b4404851216c44b0f6b4807
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-02-19 17:43:06 +0100

    don't check the fiat rates on testnets

--- utils/common.js
+++ utils/common.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
+import { devNet } from '.'
 
 export const fetchCurrentFiatRate = async (currency, setRate) => {
+  if (devNet) return 0
   const response = await axios('v2/rates/current/' + currency)
   setRate(response?.data[currency])
 }
 
 export const fetchHistoricalRate = async ({ timestamp, selectedCurrency, setPageFiatRate }) => {
-  if (!timestamp || !selectedCurrency) return
+  if (!timestamp || !selectedCurrency || devNet) return
   const response = await axios(
     'v2/rates/history/nearest/' + selectedCurrency + '?date=' + new Date(timestamp).valueOf()
   ).catch((error) => {
