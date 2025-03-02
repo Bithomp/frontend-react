799dff022ad31b96227cafbbd3a76ac71853e29f
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-03-02 10:42:47 +0100

    tx view refactor

--- components/Transaction/TransactionCard.js
+++ components/Transaction/TransactionCard.js
@@ -2,7 +2,7 @@ import CopyButton from '../UI/CopyButton'
 import { useState } from 'react'
 import { i18n, useTranslation } from 'next-i18next'
 
-import { Card, Info, Heading, MainBody, Type } from './styled'
+import { Card, Info, Type } from './styled'
 import { LedgerLink, LinkTx } from '../../utils/links'
 import { TDetails, TBody, TRow, TData } from '../TableDetails'
 import {
@@ -180,213 +180,232 @@ export const TransactionCard = ({ data, pageFiatRate, selectedCurrency, txTypeSp
   const txLink = server + '/tx/' + (tx?.ctid || tx?.hash)
 
   return (
-    <MainBody>
-      <Heading>Transaction Details</Heading>
-      <Card>
-        {id === tx?.hash && (
-          <Info>
-            <span className="bold">{id}</span> <CopyButton text={id} />
-          </Info>
-        )}
-        {error_message ? (
-          <Info className="orange">{error_message}</Info>
-        ) : (
-          <>
-            {isSuccessful ? (
-              <Info>
-                The transaction was <b className="green">successful</b> and validated in the ledger{' '}
-                <LedgerLink version={outcome.ledgerIndex} /> (index: {outcome.indexInLedger}).
-              </Info>
-            ) : (
-              <Info>
-                The transaction <b className="red">FAILED</b> and included to the ledger{' '}
-                <LedgerLink version={outcome.ledgerIndex} /> (index: {outcome.indexInLedger}).
-              </Info>
-            )}
-            <TDetails>
-              <TBody>
-                {id === tx.ctid && (
-                  <TRow>
-                    <TData>Compact Tx ID</TData>
-                    <TData>
-                      <span className="bold">{tx.ctid}</span> <CopyButton text={tx.ctid} />
-                    </TData>
-                  </TRow>
-                )}
-                <TRow>
-                  <TData>{t('table.type')}</TData>
-                  <TData>
-                    <Type>{txTypeSpecial || tx.TransactionType}</Type>
-                  </TData>
-                </TRow>
-                {hookReturn && (
-                  <TRow>
-                    <TData>Hook return</TData>
-                    <TData className="orange bold">{decode(hookReturn)}</TData>
-                  </TRow>
-                )}
-                {!isSuccessful && (
-                  <>
+    <>
+      <div className="tx-body">
+        <h1 className="tx-header">Transaction Details</h1>
+        <Card>
+          {id === tx?.hash && (
+            <Info>
+              <span className="bold">{id}</span> <CopyButton text={id} />
+            </Info>
+          )}
+          {error_message ? (
+            <Info className="orange">{error_message}</Info>
+          ) : (
+            <>
+              {isSuccessful ? (
+                <Info>
+                  The transaction was <b className="green">successful</b> and validated in the ledger{' '}
+                  <LedgerLink version={outcome.ledgerIndex} /> (index: {outcome.indexInLedger}).
+                </Info>
+              ) : (
+                <Info>
+                  The transaction <b className="red">FAILED</b> and included to the ledger{' '}
+                  <LedgerLink version={outcome.ledgerIndex} /> (index: {outcome.indexInLedger}).
+                </Info>
+              )}
+              <TDetails>
+                <TBody>
+                  {id === tx.ctid && (
                     <TRow>
-                      <TData className="bold">Failure</TData>
-                      <TData className="red bold">{shortErrorCode(outcome.result)}</TData>
-                    </TRow>
-                    <TRow>
-                      <TData className="bold">Description</TData>
-                      <TData className="orange bold">{errorCodeDescription(outcome.result)}</TData>
-                    </TRow>
-                    {tx?.TransactionType === 'Payment' && specification?.source?.addressDetails?.service && (
-                      <TRow>
-                        <TData className="bold">Problem solving</TData>
-                        <TData className="bold">
-                          The transaction <span class="red">FAILED</span>, if your balance changed, contact{' '}
-                          {addressUsernameOrServiceLink(specification.source, 'address')} support.
-                        </TData>
-                      </TRow>
-                    )}
-                  </>
-                )}
-                <TRow>
-                  <TData>{isSuccessful ? 'Validated' : 'Rejected'}</TData>
-                  <TData>
-                    {timeFromNow(tx.date, i18n, 'ripple')} ({fullDateAndTime(tx.date, 'ripple')})
-                  </TData>
-                </TRow>
-                {children}
-                {tx?.AccountTxnID && (
-                  <TRow>
-                    <TData
-                      tooltip={
-                        isSuccessful
-                          ? 'This transaction is only valid as there is such a previously-sent transaction.'
-                          : 'This transaction is only valid if there is such a previously-sent transaction.'
-                      }
-                    >
-                      Previous transaction
-                    </TData>
-                    <TData>
-                      <LinkTx tx={tx?.AccountTxnID} />
-                    </TData>
-                  </TRow>
-                )}
-                <TRow>
-                  <TData>Ledger fee</TData>
-                  <TData>
-                    <span className="bold">{amountFormat(tx.Fee)}</span>
-                    {nativeCurrencyToFiat({
-                      amount: tx.Fee,
-                      selectedCurrency,
-                      fiatRate: pageFiatRate
-                    })}
-                  </TData>
-                </TRow>
-                {specification?.signer && (
-                  <TRow>
-                    <TData>Signer</TData>
-                    <TData>
-                      <AddressWithIconFilled data={specification.signer} name="address" />
-                    </TData>
-                  </TRow>
-                )}
-                {specification?.signers &&
-                  specification?.signers.map((signer, index) => (
-                    <TRow key={index}>
-                      <TData>Signer {index + 1}:</TData>
+                      <TData>Compact Tx ID</TData>
                       <TData>
-                        <AddressWithIconFilled data={signer} name="address" />
+                        <span className="bold">{tx.ctid}</span> <CopyButton text={tx.ctid} />
                       </TData>
                     </TRow>
-                  ))}
-                <TRow>
-                  <TData>Transaction link</TData>
-                  <TData>
-                    {txLink} <CopyButton text={txLink} />
-                  </TData>
-                </TRow>
-                <TRow>
-                  <TData>Show more</TData>
-                  <TData>
-                    <span className="link" onClick={() => setShowAdditionalData(!showAdditionalData)}>
-                      Additional data
-                    </span>{' '}
-                    |{' '}
-                    <span className="link" onClick={() => setShowRawData(!showRawData)}>
-                      {t('table.raw-data')}
-                    </span>{' '}
-                    |{' '}
-                    <span className="link" onClick={() => setShowRawMeta(!showRawMeta)}>
-                      Tx Metadata
-                    </span>
-                  </TData>
-                </TRow>
-                {showAdditionalData && (
-                  <>
-                    {tx?.TransactionType !== 'UNLReport' && (
-                      <>
-                        {tx.TicketSequence ? (
-                          <TRow>
-                            <TData>Ticket sequence</TData>
-                            <TData>#{tx.TicketSequence}</TData>
-                          </TRow>
-                        ) : (
-                          <TRow>
-                            <TData>Sequence</TData>
-                            <TData>#{tx.Sequence}</TData>
-                          </TRow>
-                        )}
-                      </>
-                    )}
-                    {tx?.hash && id !== tx.hash && (
+                  )}
+                  <TRow>
+                    <TData>{t('table.type')}</TData>
+                    <TData>
+                      <Type>{txTypeSpecial || tx.TransactionType}</Type>
+                    </TData>
+                  </TRow>
+                  {hookReturn && (
+                    <TRow>
+                      <TData>Hook return</TData>
+                      <TData className="orange bold">{decode(hookReturn)}</TData>
+                    </TRow>
+                  )}
+                  {!isSuccessful && (
+                    <>
                       <TRow>
-                        <TData>Transaction hash</TData>
+                        <TData className="bold">Failure</TData>
+                        <TData className="red bold">{shortErrorCode(outcome.result)}</TData>
+                      </TRow>
+                      <TRow>
+                        <TData className="bold">Description</TData>
+                        <TData className="orange bold">{errorCodeDescription(outcome.result)}</TData>
+                      </TRow>
+                      {tx?.TransactionType === 'Payment' && specification?.source?.addressDetails?.service && (
+                        <TRow>
+                          <TData className="bold">Problem solving</TData>
+                          <TData className="bold">
+                            The transaction <span class="red">FAILED</span>, if your balance changed, contact{' '}
+                            {addressUsernameOrServiceLink(specification.source, 'address')} support.
+                          </TData>
+                        </TRow>
+                      )}
+                    </>
+                  )}
+                  <TRow>
+                    <TData>{isSuccessful ? 'Validated' : 'Rejected'}</TData>
+                    <TData>
+                      {timeFromNow(tx.date, i18n, 'ripple')} ({fullDateAndTime(tx.date, 'ripple')})
+                    </TData>
+                  </TRow>
+                  {children}
+                  {tx?.AccountTxnID && (
+                    <TRow>
+                      <TData
+                        tooltip={
+                          isSuccessful
+                            ? 'This transaction is only valid as there is such a previously-sent transaction.'
+                            : 'This transaction is only valid if there is such a previously-sent transaction.'
+                        }
+                      >
+                        Previous transaction
+                      </TData>
+                      <TData>
+                        <LinkTx tx={tx?.AccountTxnID} />
+                      </TData>
+                    </TRow>
+                  )}
+                  <TRow>
+                    <TData>Ledger fee</TData>
+                    <TData>
+                      <span className="bold">{amountFormat(tx.Fee)}</span>
+                      {nativeCurrencyToFiat({
+                        amount: tx.Fee,
+                        selectedCurrency,
+                        fiatRate: pageFiatRate
+                      })}
+                    </TData>
+                  </TRow>
+                  {specification?.signer && (
+                    <TRow>
+                      <TData>Signer</TData>
+                      <TData>
+                        <AddressWithIconFilled data={specification.signer} name="address" />
+                      </TData>
+                    </TRow>
+                  )}
+                  {specification?.signers &&
+                    specification?.signers.map((signer, index) => (
+                      <TRow key={index}>
+                        <TData>Signer {index + 1}:</TData>
                         <TData>
-                          {shortHash(tx.hash, 10)} <CopyButton text={tx.hash} />
+                          <AddressWithIconFilled data={signer} name="address" />
                         </TData>
                       </TRow>
-                    )}
-                    {tx?.ctid && id !== tx.ctid && (
-                      <TRow>
-                        <TData>Compact Tx ID</TData>
-                        <TData>
-                          {tx.ctid} <CopyButton text={tx.ctid} />
-                        </TData>
-                      </TRow>
-                    )}
-                    {tx?.LastLedgerSequence && (
-                      <TRow>
-                        <TData
-                          tooltip={
-                            'The last ledger sequence number that the transaction can be included in. Specifying this field places a strict upper limit on how long the transaction can wait to be validated or rejected.'
-                          }
-                        >
-                          Last ledger
-                        </TData>
-                        <TData>
-                          #{tx.LastLedgerSequence} ({waitLedgers} {waitLedgers === 1 ? 'ledger' : 'ledgers'})
-                        </TData>
-                      </TRow>
-                    )}
-                    {tx?.NetworkID && (
-                      <TRow>
-                        <TData tooltip="The network ID of the chain this transaction is intended for.">
-                          Network ID
-                        </TData>
-                        <TData>{tx.NetworkID}</TData>
-                      </TRow>
-                    )}
-                  </>
-                )}
-              </TBody>
-            </TDetails>
-            <div className={'slide ' + (showRawData ? 'opened' : 'closed')} style={{ margin: '0 15px' }}>
-              {codeHighlight(tx)}
-            </div>
-            <div className={'slide ' + (showRawMeta ? 'opened' : 'closed')} style={{ margin: '0 15px' }}>
-              {codeHighlight(meta)}
-            </div>
-          </>
-        )}
-      </Card>
-    </MainBody>
+                    ))}
+                  <TRow>
+                    <TData>Transaction link</TData>
+                    <TData>
+                      {txLink} <CopyButton text={txLink} />
+                    </TData>
+                  </TRow>
+                  <TRow>
+                    <TData>Show more</TData>
+                    <TData>
+                      <span className="link" onClick={() => setShowAdditionalData(!showAdditionalData)}>
+                        Additional data
+                      </span>{' '}
+                      |{' '}
+                      <span className="link" onClick={() => setShowRawData(!showRawData)}>
+                        {t('table.raw-data')}
+                      </span>{' '}
+                      |{' '}
+                      <span className="link" onClick={() => setShowRawMeta(!showRawMeta)}>
+                        Tx Metadata
+                      </span>
+                    </TData>
+                  </TRow>
+                  {showAdditionalData && (
+                    <>
+                      {tx?.TransactionType !== 'UNLReport' && (
+                        <>
+                          {tx.TicketSequence ? (
+                            <TRow>
+                              <TData>Ticket sequence</TData>
+                              <TData>#{tx.TicketSequence}</TData>
+                            </TRow>
+                          ) : (
+                            <TRow>
+                              <TData>Sequence</TData>
+                              <TData>#{tx.Sequence}</TData>
+                            </TRow>
+                          )}
+                        </>
+                      )}
+                      {tx?.hash && id !== tx.hash && (
+                        <TRow>
+                          <TData>Transaction hash</TData>
+                          <TData>
+                            {shortHash(tx.hash, 10)} <CopyButton text={tx.hash} />
+                          </TData>
+                        </TRow>
+                      )}
+                      {tx?.ctid && id !== tx.ctid && (
+                        <TRow>
+                          <TData>Compact Tx ID</TData>
+                          <TData>
+                            {tx.ctid} <CopyButton text={tx.ctid} />
+                          </TData>
+                        </TRow>
+                      )}
+                      {tx?.LastLedgerSequence && (
+                        <TRow>
+                          <TData
+                            tooltip={
+                              'The last ledger sequence number that the transaction can be included in. Specifying this field places a strict upper limit on how long the transaction can wait to be validated or rejected.'
+                            }
+                          >
+                            Last ledger
+                          </TData>
+                          <TData>
+                            #{tx.LastLedgerSequence} ({waitLedgers} {waitLedgers === 1 ? 'ledger' : 'ledgers'})
+                          </TData>
+                        </TRow>
+                      )}
+                      {tx?.NetworkID && (
+                        <TRow>
+                          <TData tooltip="The network ID of the chain this transaction is intended for.">
+                            Network ID
+                          </TData>
+                          <TData>{tx.NetworkID}</TData>
+                        </TRow>
+                      )}
+                    </>
+                  )}
+                </TBody>
+              </TDetails>
+              <div className={'slide ' + (showRawData ? 'opened' : 'closed')} style={{ margin: '0 15px' }}>
+                {codeHighlight(tx)}
+              </div>
+              <div className={'slide ' + (showRawMeta ? 'opened' : 'closed')} style={{ margin: '0 15px' }}>
+                {codeHighlight(meta)}
+              </div>
+            </>
+          )}
+        </Card>
+      </div>
+      <style jsx>{`
+        .tx-body {
+          margin: 40px auto;
+          width: calc(100% - 40px);
+          max-width: 760px;
+          z-index: 1;
+          position: relative;
+        }
+        .tx-header {
+          margin: 24px 0;
+          color: var(--text-main);
+          font-size: 16px;
+          font-weight: 700;
+          text-align: left;
+          text-transform: uppercase;
+        }
+      `}</style>
+    </>
   )
 }
--- components/Transaction/styled.js
+++ components/Transaction/styled.js
@@ -1,26 +1,9 @@
 import styled from 'styled-components'
 
-export const MainBody = styled.div`
-  margin: 40px auto;
-  width: calc(100% - 40px);
-  max-width: 760px;
-  z-index: 1;
-  position: relative;
-}`
-
 export const Card = styled.div`
   border-top: 4px solid var(--accent-link);
   box-shadow: 0 1px 3px hsla(0, 0%, 0%, 0.2);
   padding: 8px;
-`
-
-export const Heading = styled.h1`
-  margin: 24px 0;
-  color: var(--text-main);
-  font-size: 16px;
-  font-weight: 700;
-  text-align: left;
-  text-transform: uppercase;
 `
 
 export const Info = styled.p`
