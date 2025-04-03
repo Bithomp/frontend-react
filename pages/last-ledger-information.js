bf3960ec23f03817435a58a31e6761f8acdd4661
Anna Lukashova <152614606+Anna15170221@users.noreply.github.com>
2025-04-03 19:28:56 +0300

    missing translation in last ledger info (#247)

--- pages/last-ledger-information.js
+++ pages/last-ledger-information.js
@@ -178,7 +178,7 @@ export default function LastLedgerInformation() {
           </p>
           {xahauNetwork && (
             <p>
-              {'Burned by Hooks' + ': '}
+              {t('last-ledger-information.burned-by-hooks') + ': '}
               {ledger?.hooksBurnedCoins && (
                 <span className="no-brake">
                   {niceNumber(ledger?.hooksBurnedCoins / 1000000, 6) + ' ' + nativeCurrency}
