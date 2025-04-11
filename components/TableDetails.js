77d93430180e133fc0e926c62f48679eccab0635
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-04-11 10:39:55 +0200

    Remove styled-components

--- components/TableDetails.js
+++ components/TableDetails.js
@@ -1,46 +1,47 @@
 import { BsInfoCircleFill } from 'react-icons/bs'
-import styled from 'styled-components'
-
-const TDataStyled = styled.td`
-  padding: 4px;
-  &:first-child {
-    color: var(--text-secondary);
-    text-align: right;
-  }
-  &:nth-child(2) {
-    word-break: break-word;
-  }
-`
-
-const ResponsiveTooltip = styled.span`
-  width: 170px;
-  text-align: left !important;
-  &::after {
-    top: 16px !important;
-  }
-  @media (max-width: 800px) {
-    left: -75px;
-    top: 28px !important;
-    &::after {
-      top: 0 !important;
-      left: 77.5px !important;
-      border-color: transparent transparent #333 transparent !important;
-      margin-top: -10px !important;
-    }
-  }
-`
 
 export const TData = ({ tooltip, className, children }) => (
-  <TDataStyled className={className}>
-    {children}
-    {tooltip && (
-      <>
-        {' '}
-        <span className="tooltip">
-          <BsInfoCircleFill style={{ marginBottom: -2 }} />
-          <ResponsiveTooltip className="tooltiptext">{tooltip}</ResponsiveTooltip>
-        </span>
-      </>
-    )}
-  </TDataStyled>
+  <>
+    <td className={className + ' tx-td'}>
+      {children}
+      {tooltip && (
+        <>
+          {' '}
+          <span className="tooltip">
+            <BsInfoCircleFill style={{ marginBottom: -2 }} />
+            <span className="responsive-tooltip tooltiptext">{tooltip}</span>
+          </span>
+        </>
+      )}
+    </td>
+    <style jsx>{`
+      .tx-td {
+        padding: 4px;
+        &:first-child {
+          color: var(--text-secondary);
+          text-align: right;
+        }
+        &:nth-child(2) {
+          word-break: break-word;
+        }
+      }
+      .responsive-tooltip {
+        width: 170px;
+        text-align: left !important;
+        &::after {
+          top: 16px !important;
+        }
+        @media (max-width: 800px) {
+          left: -75px;
+          top: 28px !important;
+          &::after {
+            top: 0 !important;
+            left: 77.5px !important;
+            border-color: transparent transparent #333 transparent !important;
+            margin-top: -10px !important;
+          }
+        }
+      }
+    `}</style>
+  </>
 )
