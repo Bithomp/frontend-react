2c031b7bb307b340c801c65350011abdc734addb
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-04-14 16:11:23 +0200

    tx: AccountSet: fix transferRate when 0

--- components/Transaction/TransactionAccountSet.js
+++ components/Transaction/TransactionAccountSet.js
@@ -96,7 +96,7 @@ export const TransactionAccountSet = ({ data, pageFiatRate, selectedCurrency }) 
           <TData className="bold" tooltip="The fee to charge when users transfer this account's tokens.">
             Transfer rate
           </TData>
-          <TData className="bold">{subtract(tx.TransferRate / 1000000000, 1) * 100} %</TData>
+          <TData className="bold">{tx.TransferRate ? subtract(tx.TransferRate / 1000000000, 1) * 100 : 0}%</TData>
         </tr>
       )}
       {tx.TickSize !== undefined && (
