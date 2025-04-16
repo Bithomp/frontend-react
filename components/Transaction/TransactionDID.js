20fb568c9f4236a10db85698de0b46796c2c8de0
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-04-16 07:49:26 +0200

    tx: Add DID transaction

---
+++ components/Transaction/TransactionDID.js
@@ -0,0 +1,20 @@
+import { TData } from '../Table'
+
+import { TransactionCard } from './TransactionCard'
+import { AddressWithIconFilled } from '../../utils/format'
+
+export const TransactionDID = ({ data, pageFiatRate, selectedCurrency }) => {
+  if (!data) return null
+  const { specification } = data
+
+  return (
+    <TransactionCard data={data} pageFiatRate={pageFiatRate} selectedCurrency={selectedCurrency}>
+      <tr>
+        <TData>Initiated by</TData>
+        <TData>
+          <AddressWithIconFilled data={specification.source} name="address" />
+        </TData>
+      </tr>
+    </TransactionCard>
+  )
+}
