5b628368e8b18aa77e4f548e1bf2e9f8d42e9c0a
juddydev <artoh1124@gmail.com>
2025-04-25 04:47:51 +0900

    fix IDs

--- pages/admin/pro/history.js
+++ pages/admin/pro/history.js
@@ -132,9 +132,9 @@ export default function History({ queryAddress, selectedCurrency, setSelectedCur
         headers: [
           { label: 'Date', key: 'timestampExport' },
           { label: 'Sent Amount', key: 'sentAmount' },
-          { label: 'Sent Currency', key: 'sentCurrency' },
+          { label: 'Sent Currency', key: 'koinlySentCurrency' },
           { label: 'Received Amount', key: 'receivedAmount' },
-          { label: 'Received Currency', key: 'receivedCurrency' },
+          { label: 'Received Currency', key: 'koinlyReceivedCurrency' },
           { label: 'Fee Amount', key: 'txFeeNumber' },
           { label: 'Fee Currency', key: 'txFeeCurrencyCode' },
           { label: 'Net Worth Amount', key: 'amountInFiats.' + selectedCurrency },
@@ -161,7 +161,7 @@ export default function History({ queryAddress, selectedCurrency, setSelectedCur
         ]
       },
       {
-        platform: "CoinTracking",
+        platform: 'CoinTracking',
         headers: [
           { label: 'Type', key: 'coinTrackingTxType' },
           { label: 'Buy Amount', key: 'receivedAmount' },
@@ -359,10 +359,14 @@ export default function History({ queryAddress, selectedCurrency, setSelectedCur
         res.activities[i].timestampExport = new Date(res.activities[i].timestamp * 1000).toISOString()
 
         res.activities[i].sentAmount = sending ? res.activities[i].amountNumber : ''
-        res.activities[i].sentCurrency = sending ? scvCurrency : ''
+        res.activities[i].sentCurrency = sending ? currency : ''
 
         res.activities[i].receivedAmount = !sending ? res.activities[i].amountNumber : ''
-        res.activities[i].receivedCurrency = !sending ? scvCurrency : ''
+        res.activities[i].receivedCurrency = !sending ? currency : ''
+
+        // For Koinly platform
+        res.activities[i].koinlySentCurrency = sending ? scvCurrency : ''
+        res.activities[i].koinlyReceivedCurrency = !sending ? scvCurrency : ''
 
         res.activities[i].netWorthCurrency = selectedCurrency.toUpperCase()
 
@@ -373,9 +377,12 @@ export default function History({ queryAddress, selectedCurrency, setSelectedCur
         res.activities[i].coinLedgerTxType = res.activities[i].amountNumber > 0 ? 'Deposit' : 'Withdrawal'
 
         // For CoinTracking platform
-        res.activities[i].coinTrackingTxType = res.activities[i].amountNumber > 0 
-          ? 'Deposit' 
-          : (Math.abs(res.activities[i].amountNumber) <= res.activities[i].txFeeNumber ? 'Other Fee' : 'Withdrawal')
+        res.activities[i].coinTrackingTxType =
+          res.activities[i].amountNumber > 0
+            ? 'Deposit'
+            : Math.abs(res.activities[i].amountNumber) <= res.activities[i].txFeeNumber
+            ? 'Other Fee'
+            : 'Withdrawal'
       }
       setData(res) // last request data
       if (options?.marker) {
