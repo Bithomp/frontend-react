c8e442e9abdc4a249a852b4e9a80291a431585b5
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-04-24 13:06:09 +0900

    tx: nftokenChanges, uriTokenChanges

--- components/Transaction/TransactionNFToken.js
+++ components/Transaction/TransactionNFToken.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { TData } from '../Table'
 
 import { TransactionCard } from './TransactionCard'
@@ -5,7 +6,161 @@ import { AddressWithIconFilled, amountFormat, nftIdLink, nftOfferLink } from '..
 
 //NFTokenAcceptOffer, NFTokenBurn, NFTokenCancelOffer, NFTokenCreateOffer, NFTokenMint, NFTokenModify
 
-export const flagList = (flags) => {
+const nftData = (change, nftInfo, txType) => {
+  return (
+    <>
+      <tr>
+        <TData>NFT</TData>
+        <TData>{nftIdLink(change.nftokenID)}</TData>
+      </tr>
+      {txType !== 'NFTokenMint' && (
+        <>
+          {nftInfo.issuer && (
+            <tr>
+              <TData>Issuer</TData>
+              <TData>
+                <AddressWithIconFilled data={nftInfo} name="issuer" />
+              </TData>
+            </tr>
+          )}
+          {nftInfo.transferFee && (
+            <tr>
+              <TData>Transfer fee</TData>
+              <TData>{nftInfo.transferFee / 1000}%</TData>
+            </tr>
+          )}
+
+          {nftInfo.flags && (
+            <tr>
+              <TData>Flag{Object.keys(nftInfo.flags).length > 1 ? 's' : ''}</TData>
+              <TData>{flagList(nftInfo.flags)}</TData>
+            </tr>
+          )}
+        </>
+      )}
+      {nftInfo.nftokenTaxon !== undefined && (
+        <tr>
+          <TData>NFT taxon</TData>
+          <TData>{nftInfo.nftokenTaxon}</TData>
+        </tr>
+      )}
+      {nftInfo.sequence !== undefined && (
+        <tr>
+          <TData>NFT serial</TData>
+          <TData>{nftInfo.sequence}</TData>
+        </tr>
+      )}
+    </>
+  )
+}
+
+const nftokenChanges = (changes, nftokens, txType) => {
+  /*
+  "nftokenChanges": {
+    "rJcEbVWJ7xFjL8J9LsbxBMVSRY2C7DU7rz": [
+      {
+        "status": "removed",
+        "nftokenID": "000B0000C124E14881533A9AFE4A5F481795C17003A9FACF16E5DA9C00000001",
+        "uri": "697066733A2F2F516D516A447644686648634D7955674441784B696734416F4D547453354A72736670694545704661334639515274"
+      }
+    ],
+    "rM3UEiJzg7nMorRhdED5savWDt1Gqb6TLw": [
+      {
+        "status": "added",
+        "nftokenID": "000B0000C124E14881533A9AFE4A5F481795C17003A9FACF16E5DA9C00000001",
+        "uri": "697066733A2F2F516D516A447644686648634D7955674441784B696734416F4D547453354A72736670694545704661334639515274"
+      }
+    ]
+  },
+  */
+  let showAll = true
+  let transfer = false
+  let addressFrom = ''
+  let addressTo = ''
+  if (txType === 'NFTokenMint' || txType === 'NFTokenAcceptOffer') {
+    showAll = false
+    if (txType === 'NFTokenAcceptOffer' && Object.keys(changes).length === 2) {
+      transfer = true
+    }
+  }
+  return (
+    <>
+      {showAll && (
+        <tr>
+          <TData>Affected accounts</TData>
+          <TData>
+            <br />
+          </TData>
+        </tr>
+      )}
+      {Object.keys(changes).map((address, i) => {
+        const change = changes[address]
+        let output = []
+        if (showAll) {
+          output.push(
+            <tr key={'h' + i}>
+              <TData>&nbsp;</TData>
+              <TData>
+                {i + 1}. <AddressWithIconFilled data={address} name="address" />
+              </TData>
+            </tr>
+          )
+        }
+        for (let i = 0; i < change.length; i++) {
+          const nftInfo = nftokens[change[i].nftokenID]
+          if (showAll) {
+            output.push(
+              <tr key={i}>
+                <TData>Change</TData>
+                <TData>
+                  <span
+                    className={
+                      change[i].status === 'added' ? 'green' : change[i].status === 'removed' ? 'red' : 'orange'
+                    }
+                  >
+                    {change[i].status}
+                  </span>{' '}
+                  NFT
+                </TData>
+              </tr>
+            )
+          }
+
+          if (transfer) {
+            //added for one, removed for another one.
+            if (change[i].status === 'added') {
+              addressTo = address
+            } else if (change[i].status === 'removed') {
+              addressFrom = address
+            }
+          } else {
+            output.push(<React.Fragment key={'t' + i}>{nftData(change[i], nftInfo, txType)}</React.Fragment>)
+          }
+        }
+        return output
+      })}
+      {transfer && (
+        <>
+          <tr>
+            <TData>Transfer from</TData>
+            <TData>
+              <AddressWithIconFilled data={addressFrom} name="address" />
+            </TData>
+          </tr>
+          <tr>
+            <TData>Transfer to</TData>
+            <TData>
+              <AddressWithIconFilled data={addressTo} name="address" />
+            </TData>
+          </tr>
+          {nftData(changes?.[addressTo][0], nftInfo, txType)}
+        </>
+      )}
+    </>
+  )
+}
+
+const flagList = (flags) => {
   /*
   "flags": {
     "burnable": false,
@@ -163,10 +318,10 @@ export const TransactionNFToken = ({ data, pageFiatRate, selectedCurrency }) => 
           <TData>{flagList(specification.flags)}</TData>
         </tr>
       )}
-      {outcome?.nftokenChanges && (
+      {outcome?.nftokenChanges?.length > 0 && (
         <tr>
-          <TData>nftokenChanges</TData>
-          <TData className="red">TODO</TData>
+          <TData>NFT changes</TData>
+          <TData>{nftokenChanges(outcome?.nftokenChanges, outcome?.affectedObjects?.nftokens, txType)}</TData>
         </tr>
       )}
       {txType === 'NFTokenCreateOffer' &&
--- components/Transaction/TransactionURIToken.js
+++ components/Transaction/TransactionURIToken.js
@@ -1,9 +1,146 @@
+import React from 'react'
 import { TData } from '../Table'
 
 import { TransactionCard } from './TransactionCard'
 import { AddressWithIconFilled, nftIdLink } from '../../utils/format'
 
 //URITokenBuy, URITokenCreateSellOffer, URITokenCancelSellOffer, URITokenBurn
+
+const nftData = (change, nftInfo, txType) => {
+  return (
+    <>
+      <tr>
+        <TData>NFT</TData>
+        <TData>{nftIdLink(change.uritokenID)}</TData>
+      </tr>
+      {txType !== 'URITokenMint' && (
+        <>
+          {nftInfo.issuer && (
+            <tr>
+              <TData>Issuer</TData>
+              <TData>
+                <AddressWithIconFilled data={nftInfo} name="issuer" />
+              </TData>
+            </tr>
+          )}
+          {nftInfo.flags && (
+            <tr>
+              <TData>Flag{Object.keys(nftInfo.flags).length > 1 ? 's' : ''}</TData>
+              <TData>{flagList(nftInfo.flags)}</TData>
+            </tr>
+          )}
+        </>
+      )}
+      {nftInfo.sequence !== undefined && (
+        <tr>
+          <TData>NFT serial</TData>
+          <TData>{nftInfo.sequence}</TData>
+        </tr>
+      )}
+    </>
+  )
+}
+
+const uritokenChanges = (changes, nftokens, txType) => {
+  /*
+    "uritokenChanges": {
+    "rGjLQjWZ1vRPzdqPXQM4jksdKQE8oRNd8T": [
+      {
+        "status": "added",
+        "uritokenID": "DB30404B34D1FEDCA500BD84F8A9AC77F18036A1E8966766BDE33595FC41CE57",
+        "uri": "68747470733A2F2F692E6B796D2D63646E2E636F6D2F656E74726965732F69636F6E732F6F726967696E616C2F3030302F3032372F3437352F53637265656E5F53686F745F323031382D31302D32355F61745F31312E30322E31355F414D2E706E67",
+        "issuer": "r3Q5KufJdkQyaLvHD22fJFVSZCqq4GczyU"
+      }
+    ]
+  },
+  */
+  let showAll = true
+  let transfer = false
+  let addressFrom = ''
+  let addressTo = ''
+  if (txType === 'URITokenBuy' || txType === 'URITokenCreateSellOffer' || txType === 'URITokenMint') {
+    showAll = false
+    if (txType === 'URITokenBuy' && Object.keys(changes).length === 2) {
+      transfer = true
+    }
+  }
+  return (
+    <>
+      {showAll && (
+        <tr>
+          <TData>Affected accounts</TData>
+          <TData>
+            <br />
+          </TData>
+        </tr>
+      )}
+      {Object.keys(changes).map((address, i) => {
+        const change = changes[address]
+        let output = []
+        if (showAll) {
+          output.push(
+            <tr key={'h' + i}>
+              <TData>&nbsp;</TData>
+              <TData>
+                {i + 1}. <AddressWithIconFilled data={address} name="address" />
+              </TData>
+            </tr>
+          )
+        }
+
+        for (let i = 0; i < change.length; i++) {
+          const nftInfo = nftokens[change[i].uri]
+          if (showAll) {
+            output.push(
+              <tr key={i}>
+                <TData>Change</TData>
+                <TData>
+                  <span
+                    className={
+                      change[i].status === 'added' ? 'green' : change[i].status === 'removed' ? 'red' : 'orange'
+                    }
+                  >
+                    {change[i].status}
+                  </span>{' '}
+                  NFT
+                </TData>
+              </tr>
+            )
+          }
+
+          if (transfer) {
+            //added for one, removed for another one.
+            if (change[i].status === 'added') {
+              addressTo = address
+            } else if (change[i].status === 'removed') {
+              addressFrom = address
+            }
+          } else {
+            output.push(<React.Fragment key={'t' + i}>{nftData(change[i], nftInfo, txType)}</React.Fragment>)
+          }
+        }
+        return output
+      })}
+      {transfer && (
+        <>
+          <tr>
+            <TData>Transfer from</TData>
+            <TData>
+              <AddressWithIconFilled data={addressFrom} name="address" />
+            </TData>
+          </tr>
+          <tr>
+            <TData>Transfer to</TData>
+            <TData>
+              <AddressWithIconFilled data={addressTo} name="address" />
+            </TData>
+          </tr>
+          {nftData(changes?.[addressTo][0], nftInfo, txType)}
+        </>
+      )}
+    </>
+  )
+}
 
 export const TransactionURIToken = ({ data, pageFiatRate, selectedCurrency }) => {
   if (!data) return null
@@ -79,10 +216,10 @@ export const TransactionURIToken = ({ data, pageFiatRate, selectedCurrency }) =>
           <TData className="orange">burnable</TData>
         </tr>
       )}
-      {outcome?.uriTokenChanges && (
+      {outcome?.uriTokenChanges?.length > 0 && txType !== 'NFTokenBurn' && (
         <tr>
-          <TData>uriTokenChanges</TData>
-          <TData className="red">TODO</TData>
+          <TData>URI Token Changes</TData>
+          <TData>{uritokenChanges(outcome?.uritokenChanges, outcome?.affectedObjects?.uritokens, txType)}</TData>
         </tr>
       )}
     </TransactionCard>
