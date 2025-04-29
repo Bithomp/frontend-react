ff99247029cd354e08c34c4f48e1a0b00fbe6076
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-04-29 19:32:03 +0200

    fixes and new page whales fees

--- pages/whales/submitters.js
+++ pages/whales/submitters.js
@@ -31,7 +31,7 @@ export async function getServerSideProps(context) {
   }
 }
 
-export default function Whales({ data }) {
+export default function WhaleSubmitters({ data }) {
   const isMobile = useIsMobile()
 
   return (
@@ -42,7 +42,7 @@ export default function Whales({ data }) {
         <h1 className="center">
           {ledgerName + ' addresses that submitted the most transactions in the last 24 hours'}
         </h1>
-        <WhalesTable isMobile={isMobile} data={data.addresses} noAmount={true} showFee={true} />
+        <WhalesTable isMobile={isMobile} data={data} noAmount={true} showFee={true} />
       </div>
     </>
   )
