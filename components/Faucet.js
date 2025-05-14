8f0649a0883c523cdd71ddc6f8aa45b14f935aaa
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-05-14 08:19:11 +0200

    Reapply "faucet and testPayment: pass sessionKey"
    This reverts commit 78e7ae949020ec73d6b2e7a5f8183d7a07cc73c9.

--- components/Faucet.js
+++ components/Faucet.js
@@ -32,7 +32,7 @@ const convertToDrops = (amount) => {
 const maxAmount = 100 // in native currency
 const defaultAmount = 10 // in native currency
 
-export default function Faucet({ account, type }) {
+export default function Faucet({ account, type, sessionTokenData }) {
   const router = useRouter()
   const { address: queryAddress, amount: queryAmount, destinationTag: queryDestinationTag } = router.query
 
@@ -149,6 +149,14 @@ export default function Faucet({ account, type }) {
 
     if (!testPayment) {
       data.amount = amount
+    }
+
+    if (sessionTokenData) {
+      if (testPayment) {
+        data.testPaymentSessionToken = sessionTokenData.testPaymentSessionToken
+      } else {
+        data.faucetSessionToken = sessionTokenData.faucetSessionToken
+      }
     }
 
     const endPoint = testPayment ? 'v2/testPayment' : 'v2/faucet'
--- pages/faucet.js
+++ pages/faucet.js
@@ -8,18 +8,44 @@ import { devNet, ledgerName, nativeCurrency } from '../utils'
 import NetworkTabs from '../components/Tabs/NetworkTabs'
 import Ads from '../components/Layout/Ads'
 import { useTranslation } from 'next-i18next'
+import { axiosServer, passHeaders } from '../utils/axios'
 
 export async function getServerSideProps(context) {
-  const { locale } = context
+  const { locale, req } = context
+
+  let sessionTokenData = null
+
+  const res = await axiosServer({
+    method: 'get',
+    url: devNet ? 'xrpl/faucet' : 'xrpl/testPayment',
+    headers: passHeaders(req)
+  }).catch(() => {
+    console.error('Axios server error: ', devNet ? 'xrpl/faucet' : 'xrpl/testPayment')
+  })
+  if (res?.data) {
+    sessionTokenData = res.data
+  }
+  /*
+    {
+      faucetSessionToken: xx,
+      faucetSessionTokenExpiredAt: 12
+    }
+    {
+      testPaymentSessionToken: xx,
+      testPaymentSessionTokenExpiredAt: 12
+    }
+  */
+
   return {
     props: {
       isSsrMobile: getIsSsrMobile(context),
+      sessionTokenData: sessionTokenData || null,
       ...(await serverSideTranslations(locale, ['common', 'faucet']))
     }
   }
 }
 
-export default function FaucetPage({ account, showAds }) {
+export default function FaucetPage({ account, showAds, sessionTokenData }) {
   const { t } = useTranslation()
 
   return (
@@ -28,7 +54,7 @@ export default function FaucetPage({ account, showAds }) {
       <div className="content-text content-center">
         <h1 className="center">{t('menu.developers.faucet')}</h1>
         <NetworkTabs />
-        <Faucet account={account} type={devNet ? 'faucet' : 'testPayment'} />
+        <Faucet account={account} type={devNet ? 'faucet' : 'testPayment'} sessionTokenData={sessionTokenData} />
       </div>
       <Ads showAds={showAds} />
       <br />
