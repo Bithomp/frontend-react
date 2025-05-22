132c3c2bcda363b5a56efbff6ba2ca7046280a46
Pavel Fokin <fokinpv@gmail.com>
2025-05-22 09:56:48 +0200

    Reduce Sentry trace sampling rate on the server (#291)
    Co-authored-by: Pavel Fokin <pavel.fokin.dev@gmail.com>

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -8,7 +8,7 @@ Sentry.init({
   dsn: "https://d3b1dd32720f876c640f4e6287754251@o4509359897116672.ingest.de.sentry.io/4509359897509968",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
