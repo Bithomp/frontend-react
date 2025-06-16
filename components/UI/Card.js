0c4b34ace631580a16361a766024ae5c9ce4dc32
Pavel Fokin <pavel.fokin.dev@gmail.com>
2025-06-16 14:14:44 +0200

    Add Card component

---
+++ components/UI/Card.js
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types'
+
+import styles from '@/styles/components/card.module.scss'
+
+export default function Card({ 
+    children, 
+    className,
+    as,
+    href,
+    ...rest
+}) {
+    const Component = as || (href ? 'a' : 'div');
+    const isInteractive = !!(href || rest.onClick);
+
+    // Prevent accidental form submissions when Card is rendered as a button.
+    const type = Component === 'button' ? 'button' : undefined;
+
+    return (
+        <Component 
+            className={`${styles.card} ${className || ''}`.trim()}
+            href={href}
+            type={type}
+            // An interactive card should be focusable.
+            // Let developers override this if needed.
+            tabIndex={isInteractive && rest.tabIndex === undefined ? 0 : rest.tabIndex}
+            {...rest}
+        >
+            {children}
+        </Component>
+    )
+}
+
+Card.propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    // The component to render as.
+    // @default 'div' or 'a' if 'href' is present.
+    as: PropTypes.elementType,
+
+    // If provided, the card will render as an `<a>` tag.
+    href: PropTypes.string,
+}
+
+Card.defaultProps = {
+    className: '',
+    as: undefined,
+    href: undefined
+}
