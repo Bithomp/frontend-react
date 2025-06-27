f075b4469c7a7a83b68529d11dd71ebae1f7c08f
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-06-27 15:49:27 +0200

    Add link to transactions

--- components/Account/RecentTransactions.js
+++ components/Account/RecentTransactions.js
@@ -21,7 +21,10 @@ export default function RecentTransactions({ userData, ledgerTimestamp }) {
     if (!address) return
     setLoading(true)
     setError(null)
-    const res = await axios(`/v3/transactions/${address}?limit=5` + (ledgerTimestamp ? '&toDate=' + new Date(ledgerTimestamp).toISOString() : '')).catch((error) => {
+    const res = await axios(
+      `/v3/transactions/${address}?limit=5` +
+        (ledgerTimestamp ? '&toDate=' + new Date(ledgerTimestamp).toISOString() : '')
+    ).catch((error) => {
       setError(error.message)
       setLoading(false)
     })
@@ -43,34 +46,44 @@ export default function RecentTransactions({ userData, ledgerTimestamp }) {
       <table className="table-details hide-on-small-w800">
         <thead>
           <tr>
-            <th colSpan="100">Recent transactions {historicalTitle}</th>
+            <th colSpan="100">
+              Last 5 transactions [<a href={'/explorer/' + address}>View all</a>] {historicalTitle}
+            </th>
           </tr>
         </thead>
         <tbody>
-          {loading && <tr><td colSpan="100">Loading recent transactions...</td></tr>}
-          {error && <tr><td colSpan="100" className="red">Error: {error}</td></tr>}
-          {!loading && !error && 
+          {loading && (
+            <tr>
+              <td colSpan="100">Loading recent transactions...</td>
+            </tr>
+          )}
+          {error && (
+            <tr>
+              <td colSpan="100" className="red">
+                Error: {error}
+              </td>
+            </tr>
+          )}
+          {!loading && !error && (
             <>
               <tr>
                 <th>#</th>
-                <th className="left">Time</th>
+                <th className="right">Validated</th>
                 <th className="right">Type</th>
                 <th className="right">Hash</th>
-                <th className="center">Status</th>
+                <th className="right">Status</th>
               </tr>
               {transactions.map((tx, i) => (
                 <tr key={tx.txHash || tx.tx?.hash || i}>
-                  <td className="center" style={{ width: 30 }}>{i + 1}</td>
-                  <td className="left">
-                    {tx.tx?.date ? timeOrDate(tx.tx.date + 946684800) : '-'}
+                  <td className="center" style={{ width: 30 }}>
+                    {i + 1}
+                  </td>
+                  <td className="right">{tx.tx?.date ? timeOrDate(tx.tx.date + 946684800) : '-'}</td>
+                  <td className="right">{tx.tx?.TransactionType}</td>
+                  <td className="right">
+                    <LinkTx tx={tx.txHash || tx.tx?.hash} /> <CopyButton text={tx.txHash || tx.tx?.hash} />
                   </td>
                   <td className="right">
-                    {tx.tx?.TransactionType}
-                  </td>
-                  <td className="right">
-                    <LinkTx tx={tx.txHash || tx.tx?.hash}/> <CopyButton text={tx.txHash || tx.tx?.hash} />                    
-                  </td>
-                  <td className="center">
                     <span className={tx.outcome?.result === 'tesSUCCESS' ? 'green' : 'red'}>
                       {tx.outcome?.result === 'tesSUCCESS' ? 'Success' : 'Failed'}
                     </span>
@@ -78,46 +91,43 @@ export default function RecentTransactions({ userData, ledgerTimestamp }) {
                 </tr>
               ))}
             </>
-          }
+          )}
         </tbody>
       </table>
       <div className="show-on-small-w800">
         <br />
         <center>
-          {'Recent Transactions'.toUpperCase()}
-          {historicalTitle}
+          {'Last 5 Transactions'.toUpperCase()} [<a href={'/explorer/' + address}>View all</a>]{historicalTitle}
         </center>
         <br />
         {loading && <span className="grey">Loading recent transactions...</span>}
-        {error && <span className="red">Error: {error}</span>}        
+        {error && <span className="red">Error: {error}</span>}
         {!loading && !error && transactions.length > 0 && (
           <table className="table-mobile wide">
             <tbody>
               <tr>
                 <th>#</th>
-                <th className="left">Time</th>
+                <th className="right">Validated</th>
                 <th className="right">Type</th>
-                <th className="right">Link</th>
-                <th className="center">Status</th>
+                <th className="center">Link</th>
+                <th className="left">Status</th>
               </tr>
               {transactions.map((tx, i) => (
                 <tr key={tx.txHash || tx.tx?.hash || i}>
-                  <td className="center" style={{ width: 30 }}>{i + 1}</td>
-                  <td className="left">
-                    {tx.tx?.date ? timeOrDate(tx.tx.date + 946684800) : '-'}
+                  <td className="center" style={{ width: 30 }}>
+                    {i + 1}
                   </td>
-                  <td className="right">
-                    {tx.tx?.TransactionType}
-                  </td>
-                  <td className="right">
-                    <LinkTx tx={tx.txHash || tx.tx?.hash} icon={true}/>
-                  </td>
+                  <td className="right">{tx.tx?.date ? timeOrDate(tx.tx.date + 946684800) : '-'}</td>
+                  <td className="right">{tx.tx?.TransactionType}</td>
                   <td className="center">
+                    <LinkTx tx={tx.txHash || tx.tx?.hash} icon={true} />
+                  </td>
+                  <td className="left">
                     <span className={tx.outcome?.result === 'tesSUCCESS' ? 'green' : 'red'}>
                       {tx.outcome?.result === 'tesSUCCESS' ? 'Success' : 'Failed'}
                     </span>
                   </td>
-                </tr>              
+                </tr>
               ))}
             </tbody>
           </table>
@@ -125,4 +135,4 @@ export default function RecentTransactions({ userData, ledgerTimestamp }) {
       </div>
     </>
   )
-} 
+}
