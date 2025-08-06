e5ebe8d9d144d1b35a7c9270b80fc578ac86b84e
pandablue0809 <bluep0415@gmail.com>
2025-08-07 06:41:28 +0900

    add balanceChage

--- components/Transactions/TransactionRowCard.js
+++ components/Transactions/TransactionRowCard.js
@@ -1,5 +1,7 @@
-import { amountFormat, dateFormat, nativeCurrencyToFiat, timeFormat } from '../../utils/format'
+import { amountFormat, dateFormat, nativeCurrencyToFiat, timeFormat, addressUsernameOrServiceLink } from '../../utils/format'
 import { LinkTx } from '../../utils/links'
+import { dappBySourceTag, errorCodeDescription, shortErrorCode } from '../../utils/transaction'
+import { nativeCurrency } from '../../utils'
 import { FiCalendar, FiClock } from 'react-icons/fi'
 import { FaArrowRightArrowLeft } from 'react-icons/fa6'
 
@@ -12,15 +14,18 @@ export const TransactionRowCard = ({
   selectedCurrency, 
   pageFiatRate
 }) => {
-  const { specification, tx } = data
+  const { specification, tx, outcome } = data
   console.log(data, address)
   const date = dateFormat(tx.date + 946684800)
   const time = timeFormat(tx.date + 946684800)
   const memos = specification.memos
+  const myBalanceChanges = outcome?.balanceChanges?.find((change) => change.address === address)?.balanceChanges?.filter((change) => !(change.currency === nativeCurrency && change.value == -outcome.fee))
+  const isSuccessful = outcome?.result == 'tesSUCCESS'
+  console.log(myBalanceChanges)
 
   return (
-    <tr index={index}>
-      <td className="bold center" style={{width: 15}}>{index + 1}</td>      
+    <tr index={index} style={{ background: !isSuccessful ? 'repeating-linear-gradient(45deg, #f9e3b9, #f9e3b9 10px, #fff 10px, #fff 20px)' : '' }}>
+      <td className="bold center" style={{width: 10}}>{index + 1}</td>      
       <td className="left" style={{width: 100}}>
         <span className="flex items-center gap-1"><FiCalendar style={{ stroke: '#666' }} /> {date}</span>
         <span className="flex items-center gap-1"><FiClock style={{ stroke: '#666' }} /> {time}</span>
@@ -34,6 +39,15 @@ export const TransactionRowCard = ({
         <span className="bold">{txTypeSpecial || tx?.TransactionType}</span>
         <br />
         {children}
+        {outcome && !isSuccessful && (
+          <>
+            <span className="bold">Failure</span>
+            <span className="red bold">{shortErrorCode(outcome.result)}</span>
+            <br />
+            <span className="bold">Description: </span>
+            <span className="orange bold">{errorCodeDescription(outcome.result)}</span>
+          </>
+        )}
         {tx.DestinationTag && (
           <>
             <span className="gray">Destination tag: {tx.DestinationTag}</span>
@@ -42,7 +56,7 @@ export const TransactionRowCard = ({
         )}
         {tx.SourceTag && (
           <>
-            <span className="gray">Source tag: {tx.SourceTag}</span>
+            <span className="gray">Source tag: {dappBySourceTag(tx.SourceTag)}</span>
             <br />
           </>
         )}
@@ -68,6 +82,24 @@ export const TransactionRowCard = ({
 
       </td>
       <td className="right">
+        {myBalanceChanges?.map((c, i) => {
+          return (
+            <div key={i}>
+              <span className={'bold tooltip ' + (Number(c.value) > 0 ? 'green' : 'red')}>
+                {amountFormat(c, { short: true, maxFractionDigits: 2 })}
+                <span className="tooltiptext">{amountFormat(c, { precise: true })}</span>
+              </span>
+              {c.issuer && (
+                <>({addressUsernameOrServiceLink(c, 'issuer', { short: true })})</>
+              )}
+              {nativeCurrencyToFiat({
+                amount: c,
+                selectedCurrency,
+                fiatRate: pageFiatRate
+              })}
+            </div>
+          )
+        })}
         <span>Fee:</span> 
         <span className="bold">{amountFormat(tx.Fee)}</span>
         <span>
