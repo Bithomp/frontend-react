62cb81ff3071be11a1a7ee71d025284394b04d91
pandablue0809 <bluep0415@gmail.com>
2025-08-07 22:08:24 +0900

    add check type

--- components/Transactions/TransactionRowCheck.js
+++ components/Transactions/TransactionRowCheck.js
@@ -1,6 +1,7 @@
 import { TransactionRowCard } from './TransactionRowCard'
 import { useEffect, useState } from 'react'
 import { fetchHistoricalRate } from '../../utils/common'
+import { amountFormat, nativeCurrencyToFiat, addressUsernameOrServiceLink } from '../../utils/format'
 
 export const TransactionRowCheck = ({ tx, address, index, selectedCurrency}) => {
   const [pageFiatRate, setPageFiatRate] = useState(0)
@@ -14,6 +15,10 @@ export const TransactionRowCheck = ({ tx, address, index, selectedCurrency}) => 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedCurrency, tx])
 
+  const { outcome } = tx
+
+  const checkChanges = outcome?.checkChanges
+
   return (
     <TransactionRowCard
       data={tx}
@@ -22,7 +27,22 @@ export const TransactionRowCheck = ({ tx, address, index, selectedCurrency}) => 
       pageFiatRate={pageFiatRate}
       selectedCurrency={selectedCurrency}
     >
-      {/* Check */}
+      {checkChanges.sendMax && (
+        <div>
+          <span>Max amount: </span>
+          <span>
+            <span className="bold orange">{amountFormat(checkChanges.sendMax)}</span>
+            {checkChanges.sendMax?.issuer && (
+              <>({addressUsernameOrServiceLink(checkChanges.sendMax, 'issuer', { short: true })})</>
+            )}
+            {nativeCurrencyToFiat({
+              amount: checkChanges.sendMax,
+              selectedCurrency,
+              fiatRate: pageFiatRate
+            })}
+          </span>
+        </div>
+      )}
     </TransactionRowCard>
   )
 }
