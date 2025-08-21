1b765674ae4794c8922501b3e6ae1317c9ff3789
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-08-21 14:25:46 +0300

    add examples

--- pages/explorer2.js
+++ pages/explorer2.js
@@ -3,12 +3,14 @@ import { useTranslation } from 'next-i18next'
 import Head from 'next/head'
 import Link from 'next/link'
 
-import { server, explorerName, nativeCurrency } from '../utils'
+import { server, explorerName, nativeCurrency, network } from '../utils'
 import { getIsSsrMobile } from '../utils/mobile'
 
 import SEO from '../components/SEO'
 import SearchBlock from '../components/Layout/SearchBlock'
 import Ads from '../components/Layout/Ads'
+import CopyButton from '../components/UI/CopyButton'
+import { shortHash } from '../utils/format'
 
 export async function getServerSideProps(context) {
   const { locale } = context
@@ -30,14 +32,37 @@ const ldJsonWebsite = {
     '@type': 'SearchAction',
     target: {
       '@type': 'EntryPoint',
-      urlTemplate: server + '/explorer2/?r={search_term_string}'
+      urlTemplate: server + '/explorer/{search_term_string}'
     },
     'query-input': 'required name=search_term_string'
   }
 }
 
-export default function Explorer2({ isSsrMobile, showAds }) {
+const examples = {
+  mainnet: {
+    nft: '000A13884B50699E253C5098DEFE3A0872A79D129172F496F5F7E0EA00000532',
+    amm: '160C6649399D6AF625ED94A66812944BDA1D8993445A503F6B5730DECC7D3767',
+    txHash: '29C56EB4A9E6C6F16A54968EC7DC8DAE92A95348EC583F2B82A028C3EAE627C0',
+    txCTID: 'C5DB956000090000',
+    account: 'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh',
+    payString: ['username$paystring.crypto.com', 'vbuterin$bithomp.com'],
+    xAddress: 'XVVFXHFdehYhofb7XRWeJYV6kjTEwbq2mLScCiYyDTHKu9E',
+    username: ['bitstamp', 'vbuterin', 'JoelKatz'],
+    object: '0802E4E7EACC17414747174474487E9DF24CFDB61DD12C58B09D9EFB42C7F8C8'
+  },
+  testnet: {}
+}
+
+export default function Explorer({ isSsrMobile, showAds }) {
   const { t } = useTranslation()
+
+  const shortingHash = (hash) => {
+    let length = 24
+    if (isSsrMobile) {
+      length = 7
+    }
+    return shortHash(hash, length)
+  }
 
   return (
     <>
@@ -59,51 +84,53 @@ export default function Explorer2({ isSsrMobile, showAds }) {
       <section className="home-section">
         <div className="content-text content-center">
           <h2 className="center">Examples</h2>
-          <p className="center">Try these to see what you can find:</p>
-          <ul>
-            <li>
-              NFT:{' '}
-              <Link href={'/nft/' + '0000000000000000000000000000000000000000000000000000000000000000'}>
-                /nft/000000...0000
-              </Link>
-            </li>
-            <li>
-              AMM:{' '}
-              <Link href={'/amm/' + '0000000000000000000000000000000000000000'}>/amm/000000...0000</Link>
-            </li>
-            <li>
-              Transaction:{' '}
-              <Link href={'/tx/' + '0000000000000000000000000000000000000000000000000000000000000000'}>
-                /tx/000000...0000
-              </Link>
-            </li>
-            <li>
-              Account:{' '}
-              <Link href={'/account/' + 'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh'}>
-                /account/rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh
-              </Link>
-            </li>
-            <li>
-              X-address:{' '}
-              <Link href={'/account/' + 'X7u8pEExampleXAddress0000000000000000000'}>
-                /account/X7u8pEExampleXAddress...
-              </Link>
-            </li>
-            <li>
-              PayString:{' '}
-              <Link href={'/account/' + 'ihomp$bithomp.com'}>/account/ihomp$bithomp.com</Link>
-            </li>
-            <li>
-              Username:{' '}
-              <Link href={'/account/' + 'ihomp'}>/account/ihomp</Link>
-            </li>
-            <li>
-              Object ID:{' '}
-              <Link href={'/object/' + '0000000000000000000000000000000000000000000000000000000000000000'}>
-                /object/000000...0000
-              </Link>
-            </li>
-          </ul>
+          <h2>Account</h2>
+          <p>
+            start typing an account address, X-address, PayString, username, service name or web domain in the search
+            box above.
+          </p>
+          <b>Address</b> examples: "rHb9CJ" - start typing it, and we will find the full address for you, or enter a
+          full address like {examples[network]?.account} <CopyButton text={examples[network]?.account} />
+          <br />
+          <br />
+          <b>Username</b> examples: {examples[network]?.username[0]}, {examples[network]?.username[1]},{' '}
+          {examples[network]?.username[2]} (start typing and we will find the full usernames){' '}
+          <Link href="/username">Register your username.</Link>
+          <br />
+          <br />
+          <b>Domain</b> examples: "binance.com", "coinbase.com", "ripple.com" (start typing and we will find accounts
+          with such domains) <Link href="/domains">Verify your domain.</Link>
+          <br />
+          <br />
+          <b>X-Address</b> example: {shortingHash(examples[network]?.xAddress)}{' '}
+          <CopyButton text={examples[network]?.xAddress} />
+          <br />
+          <br />
+          <b>PayString</b> example: {examples[network]?.payString[0]}{' '}
+          <CopyButton text={examples[network]?.payString[0]} />, {examples[network]?.payString[1]}{' '}
+          <CopyButton text={examples[network]?.payString[1]} /> <Link href="/username">Get your PayString.</Link>
+          <br />
+          <br />
+          <h2>Transaction</h2>
+          <b>CTID</b> (compact transaction ID) example: {examples[network]?.txCTID}{' '}
+          <CopyButton text={examples[network]?.txCTID} />
+          <br />
+          <br />
+          <b>Transaction Hash</b> example: {shortingHash(examples[network]?.txHash)}{' '}
+          <CopyButton text={examples[network]?.txHash} />
+          <br />
+          <br />
+          <h2>NFT, AMM, Object</h2>
+          <b>NFT</b> example: {shortingHash(examples[network]?.nft)} <CopyButton text={examples[network]?.nft} />
+          <br />
+          <br />
+          <b>AMM</b> example: {shortingHash(examples[network]?.amm)} <CopyButton text={examples[network]?.amm} />
+          <br />
+          <br />
+          <b>Object</b> example: {shortingHash(examples[network]?.object)}{' '}
+          <CopyButton text={examples[network]?.object} />
+          <br />
+          <br />
         </div>
       </section>
 
@@ -112,5 +139,3 @@ export default function Explorer2({ isSsrMobile, showAds }) {
     </>
   )
 }
-
-
