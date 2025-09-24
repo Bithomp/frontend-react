17f5901874d76221ff97576aef1ce5757bacda2c
Tronix <137486425+pandablue0809@users.noreply.github.com>
2025-09-24 19:06:18 +0900

    /token and /tokens pages price calculations (#606)
    * update price calculations in token page
    
    * fix token page past price
    
    * fixed tokens page
    
    * fix showing price by selected currency
    
    * prettify
    
    * Remove unessary requests on the token page
    
    * tokens: fixes
    
    * Update _app.js
    
    ---------
    
    Co-authored-by: Viacheslav Bakshaev <bakshaev@protonmail.ch>

--- pages/_app.js
+++ pages/_app.js
@@ -99,10 +99,19 @@ const MyApp = ({ Component, pageProps }) => {
       '/amms',
       '/distribution',
       '/admin/watchlist',
+      '/nft/[[...id]]',
       '/tokens',
-      '/nft/[[...id]]'
+      '/token/[[...id]]'
     ]
-    const skipOnFirstRender = ['/', '/account', '/account/[id]', '/account/[id]/transactions', '/amms', '/tokens']
+    const skipOnFirstRender = [
+      '/',
+      '/account',
+      '/account/[id]',
+      '/account/[id]/transactions',
+      '/amms',
+      '/tokens',
+      '/token/[[...id]]'
+    ]
 
     // Skip fetch on first render for pages that get on the server side
     if (firstRenderRef.current && skipOnFirstRender.includes(router.pathname)) {
