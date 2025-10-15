9afa8988600369a209ae6622064c284836f93fd5
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-10-15 08:49:03 +0200

    prettify

--- components/Account/DexOrdersData.js
+++ components/Account/DexOrdersData.js
@@ -20,38 +20,38 @@ export default function DexOrdersData({ account, offerList, ledgerTimestamp, set
   /*
   [
     {
-        "Account": "rwietsevLFg8XSmG3bEZzFein1g8RBqWDZ",
-        "BookDirectory": "DF3B53548CA2E6F1211A220D4146494E0AC99D6AEDDC5F005D2386F26FC0FFF7",
-        "BookNode": "0",
-        "Flags": 131072,
-        "LedgerEntryType": "Offer",
-        "OwnerNode": "3",
-        "PreviousTxnID": "02E09F4A7FBF3E76375E472ED249BF97B7E117CFA513C7D05979B54415FDE8BC",
-        "PreviousTxnLgrSeq": 78942270,
-        "Sequence": 1561,
-        "TakerGets": {
-            "currency": "POO",
-            "issuer": "rshitKoinuxBNACMhL6QLwAsfWCUDxYobm",
-            "value": "98900.97291755109",
-            "issuerDetails": {
-                "address": "rshitKoinuxBNACMhL6QLwAsfWCUDxYobm",
-                "username": null,
-                "service": null
-            }
-        },
-        "TakerPays": "98900972917551",
-        "index": "F9A23DEDE38BAB7E4A6DA7A37DB433FD8F77EA000DC12FF575358A8DBF427C44",
-        "flags": {
-            "passive": false,
-            "sell": true
-        },
-        "accountDetails": {
-            "address": "rwietsevLFg8XSmG3bEZzFein1g8RBqWDZ",
-            "username": null,
-            "service": "XRPL-Labs"
-        },
-        "quality": "999999999.9999991",
-        "previousTxAt": 1680873430
+      "Account": "rwietsevLFg8XSmG3bEZzFein1g8RBqWDZ",
+      "BookDirectory": "DF3B53548CA2E6F1211A220D4146494E0AC99D6AEDDC5F005D2386F26FC0FFF7",
+      "BookNode": "0",
+      "Flags": 131072,
+      "LedgerEntryType": "Offer",
+      "OwnerNode": "3",
+      "PreviousTxnID": "02E09F4A7FBF3E76375E472ED249BF97B7E117CFA513C7D05979B54415FDE8BC",
+      "PreviousTxnLgrSeq": 78942270,
+      "Sequence": 1561,
+      "TakerGets": {
+        "currency": "POO",
+        "issuer": "rshitKoinuxBNACMhL6QLwAsfWCUDxYobm",
+        "value": "98900.97291755109",
+        "issuerDetails": {
+          "address": "rshitKoinuxBNACMhL6QLwAsfWCUDxYobm",
+          "username": null,
+          "service": null
+        }
+      },
+      "TakerPays": "98900972917551",
+      "index": "F9A23DEDE38BAB7E4A6DA7A37DB433FD8F77EA000DC12FF575358A8DBF427C44",
+      "flags": {
+        "passive": false,
+        "sell": true
+      },
+      "accountDetails": {
+        "address": "rwietsevLFg8XSmG3bEZzFein1g8RBqWDZ",
+        "username": null,
+        "service": "XRPL-Labs"
+      },
+      "quality": "999999999.9999991",
+      "previousTxAt": 1680873430
     }
   ]
   */
@@ -60,15 +60,10 @@ export default function DexOrdersData({ account, offerList, ledgerTimestamp, set
     const sell = offer.flags?.sell
     return (
       <tr key={i}>
-        <td
-          className="left flex align-items-center gap-2"
-          style={{ width: '100%' }}
-        >
+        <td className="left flex align-items-center gap-2" style={{ width: '100%' }}>
           <span className="tooltip">
             <span className={sell ? 'red' : 'green'}>{sell ? 'Selling ' : 'Buying '}</span>
-            <span className="tooltiptext no-brake">
-              Sequence: {offer.Sequence}
-            </span>
+            <span className="tooltiptext no-brake">Sequence: {offer.Sequence}</span>
           </span>
           <span className="bold flex align-items-center">
             {amountFormatWithIcon({ amount: sell ? offer.TakerGets : offer.TakerPays })}
@@ -165,13 +160,20 @@ export default function DexOrdersData({ account, offerList, ledgerTimestamp, set
   // Mobile-specific row format
   const mobileOrderRows = sortedOfferList.map((offer, i) => {
     const sell = offer.flags?.sell
-    
+
     // Format the offer details
     const offerDetails = (
       <>
-        <span className="bol flex align-items-center">{amountFormatWithIcon({ amount: sell ? offer.TakerGets : offer.TakerPays })}</span>
-        <span className="gre flex" style={{ alignItems: 'center' }}> <FaArrowRight /></span>
-        <span className="bol flex align-items-center">{amountFormatWithIcon({ amount: sell ? offer.TakerPays : offer.TakerGets })}</span>
+        <span className="bol flex align-items-center">
+          {amountFormatWithIcon({ amount: sell ? offer.TakerGets : offer.TakerPays })}
+        </span>
+        <span className="gre flex" style={{ alignItems: 'center' }}>
+          {' '}
+          <FaArrowRight />
+        </span>
+        <span className="bol flex align-items-center">
+          {amountFormatWithIcon({ amount: sell ? offer.TakerPays : offer.TakerGets })}
+        </span>
       </>
     )
 
@@ -182,60 +184,62 @@ export default function DexOrdersData({ account, offerList, ledgerTimestamp, set
           <span>1 {nativeCurrency} = </span>
           <span className="no-brake">
             {niceNumber(multiply(offer.quality, 1000000), 0, null, 5)}{' '}
-            {niceCurrency(offer.TakerPays?.currency || nativeCurrency)} 
+            {niceCurrency(offer.TakerPays?.currency || nativeCurrency)}
           </span>
         </>
       ) : typeof offer.TakerPays === 'string' ? (
         <>
           <span>1 {niceCurrency(offer.TakerGets?.currency)} = </span>
           <span className="no-brake">
-            {niceNumber(divide(offer.quality, 1000000), 0, null, 5)}{' '}
-            {nativeCurrency}
+            {niceNumber(divide(offer.quality, 1000000), 0, null, 5)} {nativeCurrency}
           </span>
         </>
       ) : (
         <>
           <span>1 {niceCurrency(offer.TakerGets?.currency)} = </span>
           <span className="no-brake">
-            {niceNumber(offer.quality, 0, null, 5)}{' '}
-            {niceCurrency(offer.TakerPays?.currency)}
+            {niceNumber(offer.quality, 0, null, 5)} {niceCurrency(offer.TakerPays?.currency)}
           </span>
         </>
       )
+    ) : typeof offer.TakerGets === 'string' ? (
+      <>
+        <span>1 {niceCurrency(offer.TakerPays?.currency)} = </span>
+        <span className="tooltip">
+          <span className="no-brake">
+            {niceNumber(divide(1, offer.quality * 1000000), 0, null, 2)} {nativeCurrency}
+          </span>
+          <span className="tooltiptext no-brake">
+            {fullNiceNumber(divide(1, offer.quality * 1000000))} {nativeCurrency}
+          </span>
+        </span>
+      </>
+    ) : typeof offer.TakerPays === 'string' ? (
+      <>
+        <span>1 {nativeCurrency} = </span>
+        <span className="tooltip">
+          <span className="no-brake">
+            {niceNumber(divide(1000000, offer.quality), 0, null, 2)} {niceCurrency(offer.TakerGets?.currency)}
+          </span>
+          <span className="tooltiptext no-brake">
+            {fullNiceNumber(divide(1000000, offer.quality))}
+            {niceCurrency(offer.TakerGets?.currency)}
+          </span>
+        </span>
+      </>
     ) : (
-      typeof offer.TakerGets === 'string' ? (
-        <>
-          <span>1 {niceCurrency(offer.TakerPays?.currency)} ={' '}</span>
-          <span className="tooltip">
-            <span className="no-brake">{niceNumber(divide(1, offer.quality * 1000000), 0, null, 2)} {nativeCurrency}</span>
-            <span className="tooltiptext no-brake">
-              {fullNiceNumber(divide(1, offer.quality * 1000000))} {nativeCurrency}
-            </span>
+      <>
+        <span>1 {niceCurrency(offer.TakerPays?.currency)} = </span>
+        <span className="tooltip">
+          <span className="no-brake">
+            {niceNumber(divide(1, offer.quality), 0, null, 2)} {niceCurrency(offer.TakerGets?.currency)}
           </span>
-        </>
-      ) : typeof offer.TakerPays === 'string' ? (
-        <>
-          <span>1 {nativeCurrency} ={' '}</span>
-          <span className="tooltip">
-            <span className="no-brake">{niceNumber(divide(1000000, offer.quality), 0, null, 2)} {niceCurrency(offer.TakerGets?.currency)}</span>
-            <span className="tooltiptext no-brake">
-              {fullNiceNumber(divide(1000000, offer.quality))}
-              {niceCurrency(offer.TakerGets?.currency)}
-            </span>
+          <span className="tooltiptext no-brake">
+            {fullNiceNumber(divide(1, offer.quality))}
+            {niceCurrency(offer.TakerGets?.currency)}
           </span>
-        </>
-      ) : (
-        <>
-          <span>1 {niceCurrency(offer.TakerPays?.currency)} ={' '}</span>
-          <span className="tooltip">
-            <span className="no-brake">{niceNumber(divide(1, offer.quality), 0, null, 2)} {niceCurrency(offer.TakerGets?.currency)}</span>
-            <span className="tooltiptext no-brake">
-              {fullNiceNumber(divide(1, offer.quality))}
-              {niceCurrency(offer.TakerGets?.currency)}
-            </span>
-          </span>
-        </>
-      )
+        </span>
+      </>
     )
 
     return (
@@ -243,9 +247,7 @@ export default function DexOrdersData({ account, offerList, ledgerTimestamp, set
         <td colSpan="4">
           <div className="mobile-dex-line1">
             <span className="mobile-dex-sequence">#{offer.Sequence}</span>
-            <span className={`mobile-dex-type ${sell ? 'red' : 'green'}`}>
-              {sell ? 'Selling' : 'Buying'}
-            </span>
+            <span className={`mobile-dex-type ${sell ? 'red' : 'green'}`}>{sell ? 'Selling' : 'Buying'}</span>
             <span className="mobile-dex-action">
               {offer.Account === account?.address ? (
                 <a
@@ -324,7 +326,9 @@ export default function DexOrdersData({ account, offerList, ledgerTimestamp, set
         </thead>
         <tbody>
           <tr>
-            <th className="left" style={{ paddingLeft: '20px' }}>Offer</th>
+            <th className="left" style={{ paddingLeft: '20px' }}>
+              Offer
+            </th>
             <th className="right">Rate</th>
             <th className="center">Action</th>
           </tr>
@@ -354,9 +358,7 @@ export default function DexOrdersData({ account, offerList, ledgerTimestamp, set
         </center>
         <br />
         <table className="table-mobile wide">
-          <tbody>
-            {mobileOrderRows}
-          </tbody>
+          <tbody>{mobileOrderRows}</tbody>
         </table>
         <br />
       </div>
