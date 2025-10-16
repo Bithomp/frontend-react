5f04a34a866baaac7fec380e9f8e4f66eab9a09e
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-10-16 08:09:21 +0200

    Add alias address/transactions

--- next.config.js
+++ next.config.js
@@ -124,6 +124,10 @@ module.exports = withBundleAnalyzer({
         destination: '/transaction'
       },
       {
+        source: '/address/:id/transactions',
+        destination: '/account/:id/transactions'
+      },
+      {
         source: '/address/:id',
         destination: '/account/:id'
       },
