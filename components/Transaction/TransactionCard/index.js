5050361f6400eb70b1832e60201ec3878daf89e4
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-10-29 20:31:34 +0100

    CTID redirect support

--- components/Transaction/TransactionCard/index.js
+++ components/Transaction/TransactionCard/index.js
@@ -71,15 +71,17 @@ export const TransactionCard = ({
       try {
         const { networkId: CTIDnetworkId } = decodeCTID(id)
         if (networksIds[CTIDnetworkId]) {
-          errorMessage =
-            'This transaction is from the ' +
-            networksIds[CTIDnetworkId].name +
-            ' network, check the details: ' +
-            networksIds[CTIDnetworkId].server +
-            '/' +
-            i18n.language +
-            '/tx/' +
-            id
+          errorMessage = (
+            <>
+              This transaction is from the <span className="bold">{networksIds[CTIDnetworkId].name}</span> network,
+              check the details:
+              <br />
+              <br />
+              <a href={networksIds[CTIDnetworkId].server + '/' + i18n.language + '/tx/' + id}>
+                {networksIds[CTIDnetworkId].server + '/' + i18n.language + '/tx/' + id}
+              </a>
+            </>
+          )
         } else {
           errorMessage =
             'This transaction is from the ' + CTIDnetworkId + ' network, ' + t('explorer.not-supported-network')
