e9efcbd46a687aaeb0794daff45d8a80eb80dee1
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-10-29 21:38:38 +0100

    sitemap: add Image-services

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -98,7 +98,8 @@ if (network === 'mainnet') {
     { loc: 'learn/the-bithomp-api', changefreq: 'monthly', priority: '0.9' },
     { loc: 'learn/xrp-xah-taxes', changefreq: 'monthly', priority: '0.9' },
     { loc: 'learn/issue-a-token', changefreq: 'monthly', priority: '0.9' },
-    { loc: 'learn/guide-for-token-issuers', changefreq: 'monthly', priority: '0.9' }
+    { loc: 'learn/guide-for-token-issuers', changefreq: 'monthly', priority: '0.9' },
+    { loc: 'learn/image-services', changefreq: 'monthly', priority: '0.6' }
   )
 }
 
@@ -140,7 +141,8 @@ function generateSiteMap(posts) {
     'learn/xrpl-article',
     'learn/amm',
     'learn/issue-a-token',
-    'learn/guide-for-token-issuers'
+    'learn/guide-for-token-issuers',
+    'learn/image-services'
   ]
   const oldPages = ['explorer/']
   const pagesWithoutTranslation = [...noTranslatedPages, ...oldPages]
