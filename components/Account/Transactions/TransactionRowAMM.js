7d664d66eb2f2ca11709bf96ee0e53697121941f
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-11-13 15:04:27 +0100

    transactions: amm types convertion and votes

--- components/Account/Transactions/TransactionRowAMM.js
+++ components/Account/Transactions/TransactionRowAMM.js
@@ -1,9 +1,31 @@
 import { TransactionRowCard } from './TransactionRowCard'
 
 export const TransactionRowAMM = ({ data, address, index, selectedCurrency }) => {
+  const { tx } = data
+
+  const ammTypeLabels = {
+    AMMCreate: 'AMM Create',
+    AMMDeposit: 'AMM Deposit',
+    AMMWithdraw: 'AMM Withdraw',
+    AMMVote: 'AMM Vote'
+  }
+
+  const txTypeSpecial = ammTypeLabels[tx?.TransactionType] // it will fallback to tx?.TransactionType if not found
+
   return (
-    <TransactionRowCard data={data} address={address} index={index} selectedCurrency={selectedCurrency}>
-      {/* AMM */}
+    <TransactionRowCard
+      data={data}
+      address={address}
+      index={index}
+      selectedCurrency={selectedCurrency}
+      txTypeSpecial={txTypeSpecial}
+    >
+      {(tx.TradingFee || tx.TradingFee === 0) && (
+        <>
+          Trading fee: <span className="bold">{tx.TradingFee}%</span>
+          <br />
+        </>
+      )}
     </TransactionRowCard>
   )
 }
