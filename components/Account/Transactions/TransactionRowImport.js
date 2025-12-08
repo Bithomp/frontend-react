ac443133d64f74f600228ea3b08e87b7bead2a8b
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2025-12-08 17:26:03 +0100

    get historical FiatRates in transactions api

--- components/Account/Transactions/TransactionRowImport.js
+++ components/Account/Transactions/TransactionRowImport.js
@@ -2,25 +2,24 @@ import { amountFormat, nativeCurrencyToFiat } from '../../../utils/format'
 import { TransactionRowCard } from './TransactionRowCard'
 
 export const TransactionRowImport = ({ data, address, index, selectedCurrency }) => {
-  const { outcome } = data
+  const { outcome, fiatRates } = data
+  const fiatRate = fiatRates?.[selectedCurrency]
   return (
     <TransactionRowCard data={data} address={address} index={index} selectedCurrency={selectedCurrency}>
-      {(fiatRate) => (
-        <div>
-          Received:{' '}
-          {amountFormat(outcome?.deliveredAmount, {
-            icon: true,
-            bold: true,
-            color: 'green',
-            precise: true
-          })}
-          {nativeCurrencyToFiat({
-            amount: outcome?.deliveredAmount,
-            selectedCurrency,
-            fiatRate
-          })}
-        </div>
-      )}
+      <div>
+        Received:{' '}
+        {amountFormat(outcome?.deliveredAmount, {
+          icon: true,
+          bold: true,
+          color: 'green',
+          precise: true
+        })}
+        {nativeCurrencyToFiat({
+          amount: outcome?.deliveredAmount,
+          selectedCurrency,
+          fiatRate
+        })}
+      </div>
     </TransactionRowCard>
   )
 }
