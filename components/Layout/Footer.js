0c624b6c55fd1905cacf61115f8af47b8f77cb22
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2026-01-13 14:45:24 +0100

    update footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -3,7 +3,10 @@ import { useTranslation } from 'next-i18next'
 import { useRef } from 'react'
 import dynamic from 'next/dynamic'
 
-import { devNet, nativeCurrency, network, xahauNetwork } from '../../utils'
+import { xahauNetwork } from '../../utils'
+import { GrMoney } from 'react-icons/gr'
+import { FaHeart, FaShoppingBag, FaPiggyBank } from 'react-icons/fa'
+import { MdLocationOn } from 'react-icons/md'
 
 const CookieMessage = dynamic(() => import('./CookieMessage'), { ssr: false })
 const SocialIcons = dynamic(() => import('./SocialIcons'), { ssr: false })
@@ -15,10 +18,8 @@ export default function Footer(
     //countryCode
   }
 ) {
-  const { t, i18n } = useTranslation()
+  const { t } = useTranslation()
   const footerRef = useRef()
-
-  const lang = i18n.language === 'default' ? 'en' : i18n.language
 
   return (
     <footer ref={footerRef}>
@@ -27,54 +28,33 @@ export default function Footer(
 
       <div className="footer-menu">
         <div className="footer-menu-column">
-          <span className="footer-menu-header">{t('menu.services.services')}</span>
-          <Link href="/username">{t('menu.usernames')}</Link>
-          <Link href="/learn/xrp-xah-taxes">{t('menu.services.tax-reports')}</Link>
-          <Link href="/submit-account-information">{t('menu.project-registration')}</Link>
-          {!devNet && <Link href="/alerts">{t('menu.price-alerts', { nativeCurrency })}</Link>}
-          <a href={'/submit/'}>{t('menu.submit-offline-tx')}</a>
-        </div>
-
-        <div className="footer-menu-column">
-          <span className="footer-menu-header">{t('menu.developers.developers')}</span>
-          <Link href="/learn/the-bithomp-api">{t('menu.developers.api')}</Link>
-          {network === 'mainnet' && (
-            <>
-              <a href={'https://test.bithomp.com/create/'}>{t('menu.developers.account-generation')}</a>
-              <a href={'https://test.bithomp.com/' + lang + '/faucet'}>{t('menu.developers.faucet')}</a>
-              <a href={'https://test.bithomp.com/tools/'}>Bithomp tools</a>
-            </>
-          )}
-          {devNet && (
-            <>
-              <a href={'/create/'}>{t('menu.developers.account-generation')}</a>
-              <Link href="/faucet">{t('menu.developers.faucet')}</Link>
-              <a href={'/tools/'}>Bithomp tools</a>
-            </>
-          )}
-          <a href="https://github.com/Bithomp">Github</a>
-          <Link href="/eaas">{t('menu.business.eaas')}</Link>
-          <Link href="/build-unl">{t('menu.business.build-unl')}</Link>
-        </div>
-        <div className="footer-menu-column">
           <span className="footer-menu-header">Bithomp</span>
-          <Link href="/learn/the-bithomp-explorer-advantages">Why Our Explorer</Link>
           <Link href="/about-us">{t('menu.company.about-us')}</Link>
-          <Link href="/advertise">{t('menu.business.advertise')}</Link>
-          <Link href="/admin/referrals">Affiliate program 💰</Link>
-          <Link href="/customer-support">{t('menu.customer-support')}</Link>
+          <Link href="/learn/the-bithomp-explorer-advantages">Why Our Explorer</Link>
           <Link href="/press">{t('menu.press')}</Link>
           <Link href="/jobs">Join our team</Link>
-          <Link href="/donate">{t('menu.donate')}</Link>
-          <Link href="/the-chain-of-blocks-summit">Chain Of Blocks Summit 🇲🇹</Link>
         </div>
         <div className="footer-menu-column">
-          <span className="footer-menu-header">{t('menu.legal')}</span>
-          <Link href="/disclaimer">{t('menu.disclaimer')}</Link>
-          <Link href="/privacy-policy">{t('menu.privacy-policy')}</Link>
-          <Link href="/terms-and-conditions">{t('menu.terms-and-conditions')}</Link>
+          <span className="footer-menu-header">Get involved</span>
+          <Link href="/advertise">{t('menu.business.advertise')}</Link>
+          <Link
+            href="/admin/referrals"
+            style={{ display: 'flex', alignItems: 'center', gap: '6px', textDecoration: 'none' }}
+          >
+            <span style={{ textDecoration: 'underline' }}>Affiliate program</span> <GrMoney />
+          </Link>
+          <Link href="/customer-support">{t('menu.customer-support')}</Link>
+          <Link href="/donate" style={{ display: 'flex', alignItems: 'center', gap: '6px', textDecoration: 'none' }}>
+            <span style={{ textDecoration: 'underline' }}>{t('menu.donate')}</span>{' '}
+            <FaHeart style={{ color: '#e74c3c' }} />
+          </Link>
+          <Link
+            href="/the-chain-of-blocks-summit"
+            style={{ display: 'flex', alignItems: 'center', gap: '6px', textDecoration: 'none' }}
+          >
+            <span style={{ textDecoration: 'underline' }}>Chain Of Blocks Summit</span> <MdLocationOn />
+          </Link>
         </div>
-
         <div className="footer-menu-column">
           <span className="footer-menu-header">{t('menu.learn-more.title')}</span>
           {!xahauNetwork && <Link href="/learn/xrpl-article">XRP, XRPL, Ripple</Link>}
@@ -84,14 +64,31 @@ export default function Footer(
           <Link href="/learn">See our learn page</Link>
         </div>
 
+        <div className="footer-menu-column">
+          <span className="footer-menu-header">{t('menu.legal')}</span>
+          <Link href="/disclaimer">{t('menu.disclaimer')}</Link>
+          <Link href="/privacy-policy">{t('menu.privacy-policy')}</Link>
+          <Link href="/terms-and-conditions">{t('menu.terms-and-conditions')}</Link>
+        </div>
+
         {!xahauNetwork && (
           <div className="footer-menu-column">
             <span className="footer-menu-header">{t('menu.sponsored.title')}</span>
-            <a href="https://bithomp.com/go/fm-buy" target="_blank" rel="noreferrer">
-              {t('menu.sponsored.buy')}
+            <a
+              href="https://bithomp.com/go/fm-buy"
+              target="_blank"
+              rel="noreferrer"
+              style={{ display: 'flex', alignItems: 'center', gap: '6px', textDecoration: 'none' }}
+            >
+              <span style={{ textDecoration: 'underline' }}>{t('menu.sponsored.buy')}</span> <FaShoppingBag />
             </a>
-            <a href="https://bithomp.com/go/fm-earn" target="_blank" rel="noreferrer">
-              {t('menu.sponsored.earn')}
+            <a
+              href="https://bithomp.com/go/fm-earn"
+              target="_blank"
+              rel="noreferrer"
+              style={{ display: 'flex', alignItems: 'center', gap: '6px', textDecoration: 'none' }}
+            >
+              <span style={{ textDecoration: 'underline' }}>{t('menu.sponsored.earn')}</span> <FaPiggyBank />
             </a>
             {/*
             <a
