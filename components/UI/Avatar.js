2d9fca1efdc8ba23cabcc1dfc06bf6c47cd79de1
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2026-02-08 22:47:35 +0100

    Avatar scale

--- components/UI/Avatar.js
+++ components/UI/Avatar.js
@@ -17,7 +17,13 @@ export default function Avatar({ src, alt = 'avatar', size = 35, style = {} }) {
         ...style
       }}
     >
-      <Image alt={alt} src={src} width={size} height={size} style={{ verticalAlign: 'middle' }} />
+      <Image
+        alt={alt}
+        src={src}
+        width={size}
+        height={size}
+        style={{ verticalAlign: 'middle', transform: 'scale(1.10)' }}
+      />
     </span>
   )
 }
