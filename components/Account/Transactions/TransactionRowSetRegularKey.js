5e797af9348a9123b7339ce9bb926d6e6c56d5da
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2026-04-01 15:51:50 +0200

    Update TransactionRowSetRegularKey.js

--- components/Account/Transactions/TransactionRowSetRegularKey.js
+++ components/Account/Transactions/TransactionRowSetRegularKey.js
@@ -1,16 +1,31 @@
 import { TransactionRowCard } from './TransactionRowCard'
 import { FiKey } from 'react-icons/fi'
+import { addressUsernameOrServiceLink } from '../../../utils/format'
+import CopyButton from '../../UI/CopyButton'
 
 export const TransactionRowSetRegularKey = ({ data, address, index, selectedCurrency }) => {
+  const { specification, tx } = data || {}
+  const regularKey = specification?.regularKey
+  const isRemoved = tx?.RegularKey === undefined && !regularKey
+  const txTypeSpecial = <span className="bold">{isRemoved ? 'Regular key removed' : 'Regular key set'}</span>
+
   return (
     <TransactionRowCard
       data={data}
       address={address}
       index={index}
       selectedCurrency={selectedCurrency}
+      txTypeSpecial={txTypeSpecial}
       icon={<FiKey style={{ color: '#9b59b6', fontSize: 20 }} title="Set Regular Key" />}
     >
-      {/* Set Regular Key */}
+      {isRemoved ? (
+        <div className="bold">Removed key</div>
+      ) : (
+        <div className="bold">
+          Key: {addressUsernameOrServiceLink(specification, 'regularKey', { short: 8 })}{' '}
+          <CopyButton text={regularKey} />
+        </div>
+      )}
     </TransactionRowCard>
   )
 }
