9dfa610ff78b3ebdb149c96be5f51e87e8666e40
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2026-05-11 15:41:01 +0200

    Add account index translations

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,6 +1,7 @@
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { getIsSsrMobile } from '../../utils/mobile'
+import { ledgerName } from '../../utils'
 import SEO from '../../components/SEO'
 import SearchBlock from '../../components/Layout/SearchBlock'
 
@@ -16,21 +17,18 @@ export async function getServerSideProps(context) {
 }
 
 export default function AccountIndex() {
-  const { t } = useTranslation()
+  const { t } = useTranslation(['common', 'account'])
 
   return (
     <>
       <SEO
         page="Account"
         title={t('explorer.header.account')}
-        description="Account details, transactions, NFTs, Tokens for XRP Ledger accounts"
+        description={t('index.seo-description', { ns: 'account', ledgerName })}
       />
       <div className="content-profile account">
         <h1 className="center">{t('explorer.header.account')}</h1>
-        <p className="center">
-          Here you will be able to see all the information about the account, including the transactions, tokens, NFTs,
-          and more.
-        </p>
+        <p className="center">{t('index.description', { ns: 'account' })}</p>
         <br />
         <SearchBlock searchPlaceholderText={t('explorer.enter-address')} tab="account" type="explorer" />
       </div>
